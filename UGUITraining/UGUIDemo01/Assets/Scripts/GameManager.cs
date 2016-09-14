43d49626983022aea28e15bd66c7e49be4fc536b
XINCGer <1790617178@qq.com>
2016-09-14 16:40:57 +0800

    UGUI练习之游戏菜单

---
+++ UGUITraining/UGUIDemo01/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿/*
+ProjectName: UGUI之游戏菜单
+Author: 马三小伙儿
+Blog: http://www.cnblogs.com/msxh/
+Github:https://github.com/XINCGer
+Date: 2016/09/14
+*/
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnStartGame(string sceneName){
+		Application.LoadLevel (sceneName);
+	}
+	public void OnStartGame(int sceneIndex){
+		Application.LoadLevel (sceneIndex);
+	}
+}
---
+++ UGUITraining/UGUIDemo01/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿/*
+ProjectName: UGUI之游戏菜单
+Author: 马三小伙儿
+Blog: http://www.cnblogs.com/msxh/
+Github:https://github.com/XINCGer
+Date: 2016/09/14
+*/
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed=90f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward*Time.deltaTime*speed);
+	}
+
+	public void ChangeSpeed(float newSpeed){
+		speed = newSpeed;
+	}
+}
