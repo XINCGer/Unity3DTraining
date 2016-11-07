fdd755009d8a6a449810dbbceb6a54957f0825a7
XINCGer <1790617178@qq.com>
2016-11-07 23:01:20 +0800

    列表滑动监听

---
+++ UGUITraining/UGUIDemo02/Assets/ScrollListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ScrollListener : MonoBehaviour,IBeginDragHandler,IEndDragHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
