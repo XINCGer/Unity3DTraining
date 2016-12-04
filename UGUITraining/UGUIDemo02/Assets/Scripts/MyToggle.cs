81576ce55dbb2a4b9e0d82067c4f0d4859b91b76
XINCGer <1790617178@qq.com>
2016-12-04 23:23:05 +0800

    拓展Toggle组件

---
+++ UGUITraining/UGUIDemo02/Assets/Scripts/MyToggle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyToggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
