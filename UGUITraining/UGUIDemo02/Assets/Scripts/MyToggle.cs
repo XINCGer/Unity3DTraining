584901375230bc70ebc79f80617f6e8627815cc9
XINCGer <1790617178@qq.com>
2016-12-05 23:51:03 +0800

    拓展Toggle组件

--- UGUITraining/UGUIDemo02/Assets/Scripts/MyToggle.cs
+++ UGUITraining/UGUIDemo02/Assets/Scripts/MyToggle.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class MyToggle : MonoBehaviour {
+public class MyToggle : MonoBehaviour
+{
 
+    public GameObject IsOnGameObject;
+    public GameObject IsOffGameObject;
+    private bool _isOn;
+    private Toggle _toggle;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    _toggle = this.GetComponent<Toggle>();
+	    _isOn = _toggle.isOn;
+        OnValueChange(_isOn);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void OnValueChange(bool isOn)
+    {
+        IsOnGameObject.SetActive(isOn);
+        IsOffGameObject.SetActive(!isOn);
+    }
 }
