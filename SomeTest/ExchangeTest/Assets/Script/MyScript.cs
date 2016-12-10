fdcbad51c97e3a1919b61ef72193add2dcdfbe67
XINCGer <1790617178@qq.com>
2016-12-11 00:42:51 +0800

    更换材质球测试

---
+++ SomeTest/ExchangeTest/Assets/Script/MyScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour
+{
+    public Material [] Material;
+    private MeshRenderer _meshRenderer;
+	// Use this for initialization
+	void Start ()
+	{
+	    _meshRenderer = this.transform.GetComponent<MeshRenderer>();
+	}
+
+
+    public void ExchangeMaterial(int index)
+    {
+        _meshRenderer.material = Material[index];
+    }
+}
