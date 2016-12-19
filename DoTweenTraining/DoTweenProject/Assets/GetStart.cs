ed1098645923497c23f2f278cfc2f878dedebf5b
XINCGer <1790617178@qq.com>
2016-12-19 23:48:17 +0800

    控制变量动画

---
+++ DoTweenTraining/DoTweenProject/Assets/GetStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GetStart : MonoBehaviour {
+
+    public Vector3 myValue = new Vector3(0,0,0);
+	// Use this for initialization
+	void Start ()
+	{
+	    DOTween.To(() => myValue, x => myValue = x, new Vector3(10, 10, 10), 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
