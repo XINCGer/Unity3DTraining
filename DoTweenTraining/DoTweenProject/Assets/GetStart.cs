042394ff511337726edf505617a0681ea449ba8f
XINCGer <1790617178@qq.com>
2016-12-26 23:46:42 +0800

    控制Cube和UI动画

--- DoTweenTraining/DoTweenProject/Assets/GetStart.cs
+++ DoTweenTraining/DoTweenProject/Assets/GetStart.cs
@@ -6,14 +6,19 @@ using DG.Tweening;
 public class GetStart : MonoBehaviour {
 
     public Vector3 myValue = new Vector3(0,0,0);
+    public Transform CubeTransform;
+    public RectTransform PanelRectTransform;
 	// Use this for initialization
 	void Start ()
 	{
-	    DOTween.To(() => myValue, x => myValue = x, new Vector3(10, 10, 10), 2);
+        //DOTween.To(() => myValue, x => myValue = x, new Vector3(10, 10, 10), 2);
+	    DOTween.To(() => myValue, x => myValue = x, new Vector3(0, 0, 0), 2);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+        //CubeTransform.position = myValue;
+	    PanelRectTransform.localPosition = myValue;
 	}
 }
