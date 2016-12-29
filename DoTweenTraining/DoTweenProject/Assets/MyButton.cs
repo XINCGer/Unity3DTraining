d12dc33d2194f333c72658128201497af70918e4
XINCGer <1790617178@qq.com>
2016-12-29 23:41:51 +0800

    快捷制作动画方式

---
+++ DoTweenTraining/DoTweenProject/Assets/MyButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MyButton : MonoBehaviour
+{
+
+    public RectTransform PanelRectTransform;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        PanelRectTransform.DOLocalMove(new Vector3(0, 0, 0), 0.35f);
+    }
+}
