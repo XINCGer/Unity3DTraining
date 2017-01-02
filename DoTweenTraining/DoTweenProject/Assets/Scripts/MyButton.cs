1b6513b0f3907dc9de3f018d264da2377de79ecf
XINCGer <1790617178@qq.com>
2017-01-02 23:11:03 +0800

    动画播放与倒放

---
+++ DoTweenTraining/DoTweenProject/Assets/Scripts/MyButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MyButton : MonoBehaviour
+{
+
+    public RectTransform PanelRectTransform;
+    private bool isIn = false;
+    private Tweener tweener;
+
+	// Use this for initialization
+	void Start () {
+        tweener = PanelRectTransform.DOLocalMove(new Vector3(0, 0, 0), 0.35f);
+        tweener.SetAutoKill(false);
+        tweener.Pause();
+	}
+
+    public void OnClick()
+    {
+        if (isIn == false)
+        {
+            tweener.PlayForward();
+            isIn = true;
+        }
+        else
+        {
+            tweener.PlayBackwards();
+            isIn = false;
+        }
+    }
+}
