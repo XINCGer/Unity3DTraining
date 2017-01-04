e26634e855b3c96330cd8b26c7e3ea8812ebad09
XINCGer <1790617178@qq.com>
2017-01-04 23:42:24 +0800

    Form Tweens

---
+++ DoTweenTraining/DoTweenProject/Assets/MyCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MyCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    transform.DOMoveX(5, 1).From();
+        //默认为ToTween,从当前位置移动到目标位置，加上From以后，表示从目标位置移动到当前位置
+        transform.DOMoveX(5, 1).From(true);
+        //设置为true时，表示相对坐标
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
