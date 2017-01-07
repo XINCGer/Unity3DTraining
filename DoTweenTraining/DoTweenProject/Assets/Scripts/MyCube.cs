76e73b63281c7296262d095934bf56c80f21fff2
XINCGer <1790617178@qq.com>
2017-01-07 23:58:07 +0800

    动画属性设置

--- DoTweenTraining/DoTweenProject/Assets/Scripts/MyCube.cs
+++ DoTweenTraining/DoTweenProject/Assets/Scripts/MyCube.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using DG.Tweening.Core;
 using UnityEngine;
 
-public class MyCube : MonoBehaviour {
-
+public class MyCube : MonoBehaviour
+{
+    private int y;
 	// Use this for initialization
 	void Start ()
 	{
@@ -12,10 +15,11 @@ public class MyCube : MonoBehaviour {
         //默认为ToTween,从当前位置移动到目标位置，加上From以后，表示从目标位置移动到当前位置
         transform.DOMoveX(5, 1).From(true);
         //设置为true时，表示相对坐标
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
 	}
 }
