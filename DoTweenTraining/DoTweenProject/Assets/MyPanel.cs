27f7506d24c27deec86203704a922386b4196435
XINCGer <1790617178@qq.com>
2017-01-08 22:30:34 +0800

    动画属性

---
+++ DoTweenTraining/DoTweenProject/Assets/MyPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MyPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Tweener _tweener = this.transform.DOLocalMoveX(0, 2);
+	    _tweener.SetEase(Ease.InExpo);
+	    _tweener.SetLoops(1);
+	    _tweener.OnComplete(delegate() { Debug.Log("动画播放完毕！");});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
