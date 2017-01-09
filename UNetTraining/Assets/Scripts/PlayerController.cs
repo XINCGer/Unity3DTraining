8c4b237a2575c473ad4a5cce9f3e4c3fd43ed523
XINCGer <1790617178@qq.com>
2017-01-09 23:26:50 +0800

    Player移动控制脚本

---
+++ UNetTraining/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float rotateSpeed=120f;
+    public float moveSpeed = 10f;
+	// Update is called once per frame
+	void Update ()
+	{
+	    float h = Input.GetAxis("Horizontal");
+	    float v = Input.GetAxis("Vertical");
+        this.transform.Rotate(Vector3.up*h*rotateSpeed*Time.deltaTime);
+        this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * v);
+	}
+}
