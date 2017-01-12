25367024aa741b7b4bb9d997f1fcbf889b32b94a
XINCGer <1790617178@qq.com>
2017-01-12 23:27:39 +0800

    修改Player移动控制和Transform同步

--- UNetTraining/Assets/Scripts/PlayerController.cs
+++ UNetTraining/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
 
     public float rotateSpeed=120f;
@@ -10,6 +11,7 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+	    if (isLocalPlayer == false) return;     //只有是localPlayer时才控制移动
 	    float h = Input.GetAxis("Horizontal");
 	    float v = Input.GetAxis("Vertical");
         this.transform.Rotate(Vector3.up*h*rotateSpeed*Time.deltaTime);
