920e5717060e9fc209a4adcdfc68d31ac9754938
XINCGer <1790617178@qq.com>
2017-01-13 23:53:13 +0800

    角色射击控制

--- UNetTraining/Assets/Scripts/PlayerController.cs
+++ UNetTraining/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : NetworkBehaviour
 
     public float rotateSpeed=120f;
     public float moveSpeed = 10f;
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
 	// Update is called once per frame
 	void Update ()
 	{
@@ -16,10 +18,27 @@ public class PlayerController : NetworkBehaviour
 	    float v = Input.GetAxis("Vertical");
         this.transform.Rotate(Vector3.up*h*rotateSpeed*Time.deltaTime);
         this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * v);
+
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        Fire();
+	    }
 	}
 
     public override void OnStartLocalPlayer()
     {
         GetComponent<MeshRenderer>().material.color=Color.blue;
+    }
+
+    private void Fire()
+    {
+       GameObject _bullet =  Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+        _bullet.GetComponent<Rigidbody>().velocity = BulletSpawn.forward*10;
+        Destroy(_bullet,2);
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        Debug.Log("1111");
     }
 }
