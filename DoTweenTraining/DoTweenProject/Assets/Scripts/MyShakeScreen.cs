e5f66a0f3cd81e708a358293cd9458441deaaf74
XINCGer <1790617178@qq.com>
2017-01-15 16:04:07 +0800

    屏幕震动效果

---
+++ DoTweenTraining/DoTweenProject/Assets/Scripts/MyShakeScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MyShakeScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+        //this.transform.DOShakePosition(1f, 1);
+	    this.transform.DOShakePosition(1, new Vector3(3, 3, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
