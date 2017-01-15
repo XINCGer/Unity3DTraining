3177cfd3d1e41c5776effb72c018ea3fcedb8142
XINCGer <1790617178@qq.com>
2017-01-15 22:00:36 +0800

    动画组件的可视化创建方式

---
+++ DoTweenTraining/DoTweenProject/Assets/Scripts/MyPanel2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MyPanel2 : MonoBehaviour
+{
+
+    private DOTweenAnimation _tweenAnimation;
+    private bool _isShow=false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _tweenAnimation = GetComponent<DOTweenAnimation>();
+	}
+
+    public void OnClick()
+    {
+        if (_isShow == false)
+        {
+            _tweenAnimation.DOPlayBackwards();
+        }
+        else
+        {
+            _tweenAnimation.DOPlayForward();
+        }
+        _isShow = !_isShow;
+    }
+}
