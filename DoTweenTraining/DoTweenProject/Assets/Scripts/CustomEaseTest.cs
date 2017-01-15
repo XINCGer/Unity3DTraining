ab87762e5e1396dcb79803be06ba131cbabfbdda
XINCGer <1790617178@qq.com>
2017-01-15 17:22:32 +0800

    自定义Ease

---
+++ DoTweenTraining/DoTweenProject/Assets/Scripts/CustomEaseTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class CustomEaseTest : MonoBehaviour
+{
+
+    public AnimationCurve Curve;
+	// Use this for initialization
+	void Start ()
+	{
+	    this.transform.DOMoveZ(-20, 2).SetEase(Curve);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
