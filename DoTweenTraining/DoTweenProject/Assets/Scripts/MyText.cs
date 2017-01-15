b7cc45f3c435bac1160483e245fd5b9058b9891e
XINCGer <1790617178@qq.com>
2017-01-15 15:27:35 +0800

    文字动画

---
+++ DoTweenTraining/DoTweenProject/Assets/Scripts/MyText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyText : MonoBehaviour
+{
+
+    private Text _text;
+	// Use this for initialization
+	void Start ()
+	{
+	    _text = this.GetComponent<Text>();
+	    _text.DOText("接下来，进入游戏第二篇章", 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
