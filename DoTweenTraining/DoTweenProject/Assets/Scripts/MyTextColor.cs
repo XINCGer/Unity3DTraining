2073613763368cbd65acb1416dff37fd603252cb
XINCGer <1790617178@qq.com>
2017-01-15 16:18:16 +0800

    颜色和透明度

---
+++ DoTweenTraining/DoTweenProject/Assets/Scripts/MyTextColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyTextColor : MonoBehaviour
+{
+
+    private Text _text;
+	// Use this for initialization
+	void Start ()
+	{
+	    _text = GetComponent<Text>();
+        //_text.DOColor(Color.red, 2);
+	    _text.DOFade(1, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
