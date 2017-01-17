944f91b152192eb11e7c613a9410b412b9c00366
XINCGer <1790617178@qq.com>
2017-01-17 23:29:08 +0800

    给人物添加血量控制

---
+++ UNetTraining/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+
+    public void OnCollisionEnter(Collision collider)
+    {
+        GameObject player = collider.gameObject;
+        Health _health = player.GetComponent<Health>();
+        if (_health != null)
+        {
+            _health.TakeDamage(damage);
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ UNetTraining/Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+    }
+}
