6f8845a4cefe486ff4ca917d97acbf1fdeca6a19
XINCGer <1790617178@qq.com>
2017-01-20 23:11:16 +0800

    血量和血条同步

--- UNetTraining/Assets/Scripts/Health.cs
+++ UNetTraining/Assets/Scripts/Health.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
 
     public const int maxHealth = 100;
+    [SyncVar(hook = "OnHealthChanged")]
     public int currentHealth = maxHealth;
     public Slider _slider;
 
     public void TakeDamage(int damage)
     {
+        if (isServer == false) return;
         currentHealth -= damage;
         if (currentHealth < 0)
         {
             currentHealth = 0;
             Debug.Log("Dead!");
         }
-        _slider.value = currentHealth/(float) maxHealth;
+    }
+
+    void OnHealthChanged(int health)
+    {
+        _slider.value = health / (float)maxHealth;
     }
 }
