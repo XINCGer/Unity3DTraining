036eb64c350481b9c69b47bbca1ca252e2e6fc79
XINCGer <1790617178@qq.com>
2017-02-12 22:32:21 +0800

    添加点击销毁功能

---
+++ Minecraft/Assets/Scripts/ClickDestory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDestory : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Minecraft/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Minecraft/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -86,7 +86,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
             if (m_cursorIsLocked)
             {
                 Cursor.lockState = CursorLockMode.Locked;
-                //Cursor.visible = false;
                 Cursor.visible = true;
             }
             else if (!m_cursorIsLocked)
