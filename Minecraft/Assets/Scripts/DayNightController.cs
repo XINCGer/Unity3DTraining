425f9bf2e4dad7a5d579cdd541d9d1476472b4cc
XINCGer <1790617178@qq.com>
2017-02-13 23:14:07 +0800

    控制我的世界中日夜的轮转

---
+++ Minecraft/Assets/Scripts/DayNightController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightController : MonoBehaviour
+{
+
+    public float rotateSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(Vector3.up*rotateSpeed*Time.deltaTime,Space.Self);
+	}
+}
