c50a20edd14e5cf820194bc37b4fcbf7f4b5ed1d
XINCGer <1790617178@qq.com>
2017-02-14 23:36:11 +0800

    添加UI和游戏控制器

---
+++ Minecraft/Assets/Scripts/LifeManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LifeManager : MonoBehaviour
+{
+
+    private Transform myTransform;
+    private Vector3 startPoint;
+    public GameObject gameOverUI;
+    private bool isGameOver;
+	// Use this for initialization
+	void Start ()
+	{
+	    myTransform = this.transform;
+	    startPoint = myTransform.position;
+	    isGameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (myTransform.position.y < -10)
+	    {
+	        GameOver();
+	    }
+	    if (Input.GetKeyDown(KeyCode.R) && isGameOver ==false)
+	    {
+	        Restart();
+	    }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+        isGameOver = true;
+    }
+
+    private void Restart()
+    {
+        myTransform.position = startPoint;
+        isGameOver = false;
+        gameOverUI.SetActive(false);
+    }
+}
