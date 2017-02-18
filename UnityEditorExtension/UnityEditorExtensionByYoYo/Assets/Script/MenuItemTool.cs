529075f165a1b370437a8b49b0de731627797a17
XINCGer <1790617178@qq.com>
2017-02-18 22:42:35 +0800

    Unity编辑器拓展-菜单栏

---
+++ UnityEditorExtension/UnityEditorExtensionByYoYo/Assets/Script/MenuItemTool.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuItemTool : MonoBehaviour {
+
+    [MenuItem("ClearMemory/ClearAll")]
+    static void ClearAll() {
+        Debug.Log("清除所有的缓存！");
+    }
+}
