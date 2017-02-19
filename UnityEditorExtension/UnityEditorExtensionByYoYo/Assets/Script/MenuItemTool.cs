216d71ded8ffd046dab60f6e24315a8f4d9a3cd6
XINCGer <1790617178@qq.com>
2017-02-19 17:36:59 +0800

    菜单栏拓展1

--- UnityEditorExtension/UnityEditorExtensionByYoYo/Assets/Script/MenuItemTool.cs
+++ UnityEditorExtension/UnityEditorExtensionByYoYo/Assets/Script/MenuItemTool.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class MenuItemTool : MonoBehaviour {
 
-    [MenuItem("ClearMemory/ClearAll")]
+    [MenuItem("ClearMemory/ClearAll",false,1)]
     static void ClearAll() {
         Debug.Log("清除所有的缓存！");
+    }
+    [MenuItem("ClearMemory/ClearOther/ClearShopInfo",false,2)]
+    static void ClearShopInfo() {
+        Debug.Log("清除商店的缓存！");
+    }
+    [MenuItem("ClearMemory/ClearOther/ClearPropInfo",false,3)]
+    static void ClearPropInfo() {
+        Debug.Log("清除属性的缓存！");
     }
 }
--- UnityEditorExtension/UnityEditorExtensionByYoYo/README.md
+++ UnityEditorExtension/UnityEditorExtensionByYoYo/README.md
@@ -1,3 +1,11 @@
 ##Unity编辑器拓展练习  
 
-> Unity编辑器拓展练习--参考泰课Yoyo课程  
+Unity编辑器拓展练习--参考泰课Yoyo课程  
+* 知识点总结     
+```C#
+// [MenuItem("ClearMemory/ClearAll",false,1)] 添加菜单栏拓展并设置优先级,false为显示面板，true为不显示面板
+[MenuItem("ClearMemory/ClearAll",false,1)]
+    static void ClearAll() {
+        Debug.Log("清除所有的缓存！");
+    }
+```
