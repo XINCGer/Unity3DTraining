121f608ed0d36c28e2eb5193c8e69d508aafabdc
XINCGer <1790617178@qq.com>
2017-02-22 22:20:17 +0800

    属性面板拓展4

---
+++ UnityEditorExtension/UnityEditorExtensionByYoYo/Assets/Editor/CameraEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Camera))]
+public class CameraEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        base.DrawDefaultInspector();
+        GUILayout.Label("******以下是拓展选项******");
+        if (GUILayout.Button("点击处理"))
+        {
+            Debug.Log("检测到点击事件！");
+        }
+    }
+}
---
+++ UnityEditorExtension/UnityEditorExtensionByYoYo/Assets/Editor/LightEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Light))]
+public class LightEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        base.DrawDefaultInspector();
+        if (GUILayout.Button("点击"))
+        {
+            Debug.Log("灯光点击响应！");
+        }
+    }
+}
