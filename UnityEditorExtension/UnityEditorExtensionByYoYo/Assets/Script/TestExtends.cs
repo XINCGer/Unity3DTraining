3a2ec3df685c1f6cbe9541dd72a2224913e36fe2
XINCGer <1790617178@qq.com>
2017-02-24 23:20:58 +0800

    其他拓展

---
+++ UnityEditorExtension/UnityEditorExtensionByYoYo/Assets/Script/TestExtends.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Script/TestExtends")]
+[RequireComponent(typeof(Rigidbody))]
+public class TestExtends : MonoBehaviour {
+
+    [ContextMenu("测试")]
+    public void TestMenu()
+    {
+        Debug.Log("输出.....");
+    }
+}
