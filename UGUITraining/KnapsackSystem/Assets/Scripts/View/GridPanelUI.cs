6bb911f389a0115a3df4a82d9b2d8d48ee4d10c6
XINCGer <1790617178@qq.com>
2017-02-26 14:55:55 +0800

    制作UI页面

---
+++ UGUITraining/KnapsackSystem/Assets/Scripts/View/GridPanelUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPanelUI : MonoBehaviour
+{
+
+    public Transform[] Grids;
+
+
+    public Transform GetEmpty()
+    {
+        for (int i = 0; i < Grids.Length; i++)
+        {
+            if (Grids[i].childCount == 0)
+            {
+                return Grids[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ UGUITraining/KnapsackSystem/Assets/Scripts/View/ItemUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUI : MonoBehaviour
+{
+
+    public Text ItemText;
+    public Image ItemImage;
+
+    public void UpdateText(string text)
+    {
+        this.ItemText.text = text;
+    }
+
+    public void UpdateImage(Sprite sprite)
+    {
+        this.ItemImage.sprite = sprite;
+    }
+}
