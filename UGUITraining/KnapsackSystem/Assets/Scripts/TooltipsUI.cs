acf5f33620ceec2e138ec877e515c16bfc782b8f
XINCGer <1790617178@qq.com>
2017-03-06 22:32:19 +0800

    制作提示框1

---
+++ UGUITraining/KnapsackSystem/Assets/Scripts/TooltipsUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipsUI : MonoBehaviour {
+
+    public Text OutlineText;
+    public Text ContentText;
+
+    public void UpdateTooltip(string text) {
+        OutlineText.text = text;
+        ContentText.text = text;
+    }
+}
