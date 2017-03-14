edfd5f5f49162dd9e31cf23c92da2992b0213062
马三小伙儿 <1790617178@qq.com>
2017-03-14 20:26:32 +0800

    Json解析练习

---
+++ NetWork&Resources/JsonDataDemo/Assets/Scripts/JsonDeserialize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+/// <summary>
+/// Json解析脚本挂在场景中JsonManager上
+/// </summary>
+public class JsonDeserialize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+        //获取Json文件
+	    TextAsset jsonData = Resources.Load<TextAsset>("JsonData");
+	    JsonObjectModel jsonObject = JsonMapper.ToObject<JsonObjectModel>(jsonData.text);
+	    foreach (var info in jsonObject.infoList)
+	    {
+	        Debug.Log(info.panelTypeString+" : "+info.path);
+	    }
+	}
+
+
+
+}
