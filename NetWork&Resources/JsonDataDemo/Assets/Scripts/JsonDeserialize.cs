8c8f6e2253efa090ea601e37fe4bbc17f4f033dd
马三小伙儿 <1790617178@qq.com>
2017-03-18 11:48:14 +0800

    LitJson练习

--- NetWork&Resources/JsonDataDemo/Assets/Scripts/JsonDeserialize.cs
+++ NetWork&Resources/JsonDataDemo/Assets/Scripts/JsonDeserialize.cs
@@ -14,56 +14,74 @@ public class JsonDeserialize : MonoBehaviour {
 	{
         /*********************** 使用自定义结构类读取Json ******************************/
         //获取Json文件
-        //TextAsset jsonData = Resources.Load<TextAsset>("JsonData");
-        //JsonObjectModel jsonObject = JsonMapper.ToObject<JsonObjectModel>(jsonData.text);
-        //foreach (var info in jsonObject.infoList)
-        //{
-        //    Debug.Log(info.panelTypeString+" : "+info.path);
-        //}
+        TextAsset jsonData = Resources.Load<TextAsset>("JsonData");
+        JsonObjectModel jsonObject = JsonMapper.ToObject<JsonObjectModel>(jsonData.text);
+        foreach (var info in jsonObject.infoList)
+        {
+            Debug.Log(info.panelTypeString+" : "+info.path);
+        }
         /*********************** 使用自定义结构类读取Json ******************************/
 
         /*********************** 使用JsonData读取Json ******************************/
-        //TextAsset jsonAsset = Resources.Load<TextAsset>("JsonData");
-        //JsonData jsonData = JsonMapper.ToObject(jsonAsset.text);
-        //Debug.LogWarning(jsonData.Count);
-        //Debug.LogWarning(jsonData.IsArray);
-        //Debug.LogWarning(jsonData.IsObject);
-        //Debug.LogWarning(jsonData.Keys);
-        //Debug.LogWarning(jsonData.ToString());
-        //Debug.LogWarning(jsonData.GetJsonType());
-        //foreach (JsonData  item in jsonData["infoList"])
-        //{
-        //    Debug.Log(item["panelTypeString"]);
-        //    Debug.Log(item["path"]);
-        //}
+        TextAsset jsonAsset = Resources.Load<TextAsset>("JsonData");
+        JsonData jsonData = JsonMapper.ToObject(jsonAsset.text);
+        Debug.LogWarning(jsonData.Count);
+        Debug.LogWarning(jsonData.IsArray);
+        Debug.LogWarning(jsonData.IsObject);
+        Debug.LogWarning(jsonData.Keys);
+        Debug.LogWarning(jsonData.ToString());
+        Debug.LogWarning(jsonData.GetJsonType());
+        foreach (JsonData item in jsonData["infoList"])
+        {
+            Debug.Log(item["panelTypeString"]);
+            Debug.Log(item["path"]);
+        }
         /*********************** 使用JsonData读取Json ******************************/
 
-        /*********************** 使用JsonReader ******************************/
-        //TextAsset jsonData = Resources.Load<TextAsset>("JsonData");
-        //JsonReader reader = new JsonReader(jsonData.text);
-        //while (reader.Read())
-        //{
-        //    string type = reader.Value != null ? reader.Value.GetType().ToString() : "None";
-        //    Debug.Log(reader.Token+" "+reader.Value+" "+type);
-        //}
-        //reader.Close();
-        /*********************** 使用JsonReader ******************************/
+        /*********************** 使用JsonData生成json ******************************/
+        JsonData jsonData = new JsonData();
+        jsonData["name"] = "马三小伙儿";
+        jsonData["age"] = 22;
+        jsonData["local"] = "Beijing";
+        string jsonString = jsonData.ToJson();
+        Debug.Log("Object to json: "+jsonString);
 
-        /*********************** 使用JsonWriter ******************************/
-        //通过JsonWriter类创建一个Json对象
-        StringBuilder sb = new StringBuilder();
-        JsonWriter writer = new JsonWriter(sb);
-        writer.WriteArrayStart();
-        writer.Write(1);
-        writer.Write(2);
-        writer.Write(3);
-        writer.WriteObjectStart();
-        writer.WritePropertyName("color");
-        writer.Write("blue");
-        writer.WriteObjectEnd();
-        writer.WriteArrayEnd();
-        Debug.Log(sb.ToString());
-        /*********************** 使用JsonWriter ******************************/
+        //对象中嵌套对象
+        JsonData data2 = new JsonData();
+        data2["name"] = "peiandsky";
+        data2["info"] = new JsonData();
+        data2["info"]["sex"] = "male";
+        data2["info"]["age"] = 28;
+        string json2 = data2.ToJson();
+        Debug.Log("Object to json: " + json2);
+        /*********************** 使用JsonData读取Json ******************************/
+
+	    /*********************** 使用JsonReader ******************************/
+        TextAsset jsonData = Resources.Load<TextAsset>("JsonData");
+        JsonReader reader = new JsonReader(jsonData.text);
+        while (reader.Read())
+        {
+            string type = reader.Value != null ? reader.Value.GetType().ToString() : "None";
+            Debug.Log(reader.Token + " " + reader.Value + " " + type);
+        }
+        reader.Close();
+	    /*********************** 使用JsonReader ******************************/
+
+	    /*********************** 使用JsonWriter ******************************/
+	    //通过JsonWriter类创建一个Json对象
+	    StringBuilder sb = new StringBuilder();
+	    JsonWriter writer = new JsonWriter(sb);
+	    writer.WriteArrayStart();
+	    writer.Write(1);
+	    writer.Write(2);
+	    writer.Write(3);
+	    writer.WriteObjectStart();
+	    writer.WritePropertyName("color");
+	    writer.Write("blue");
+	    writer.WriteObjectEnd();
+	    writer.WriteArrayEnd();
+	    Debug.Log(sb.ToString());
+	    /*********************** 使用JsonWriter ******************************/
 	}
 
 
