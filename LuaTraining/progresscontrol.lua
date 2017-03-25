33bcedeb96881b1fb5324af048c6582312fac605
马三小伙儿 <1790617178@qq.com>
2017-03-25 14:26:26 +0800

    Lua 流程控制·1

---
+++ LuaTraining/progresscontrol.lua
@@ -0,0 +1,62 @@
+--[ 定义变量 --]
+a = 10;
+
+--[ 使用 if 语句 --]
+if( a < 20 )
+then
+   --[ if 条件为 true 时打印以下信息 --]
+   print("a less than 20" );
+end
+print("a value is:", a);
+
+--[ 定义变量 --]
+a = 100;
+--[ 检查条件 --]
+if( a < 20 )
+then
+   --[ if 条件为 true 时执行该语句块 --]
+   print("a less than 20" )
+else
+   --[ if 条件为 false 时执行该语句块 --]
+   print("a geater than 20" )
+end
+print("a value is :", a)
+
+--[ 定义变量 --]
+a = 100
+
+--[ 检查布尔条件 --]
+if( a == 10 )
+then
+   --[ 如果条件为 true 打印以下信息 --]
+   print("a 的值为 10" )
+elseif( a == 20 )
+then   
+   --[ if else if 条件为 true 时打印以下信息 --]
+   print("a 的值为 20" )
+elseif( a == 30 )
+then
+   --[ if else if condition 条件为 true 时打印以下信息 --]
+   print("a 的值为 30" )
+else
+   --[ 以上条件语句没有一个为 true 时打印以下信息 --]
+   print("没有匹配 a 的值" )
+end
+print("a 的真实值为: ", a )
+
+--[ 定义变量 --]
+a = 100;
+b = 200;
+
+--[ 检查条件 --]
+if( a == 100 )
+then
+   --[ if 条件为 true 时执行以下 if 条件判断 --]
+   if( b == 200 )
+   then
+      --[ if 条件为 true 时执行该语句块 --]
+      print("a 的值为 100 b 的值为 200" );
+   end
+end
+print("a 的值为 :", a );
+print("b 的值为 :", b );
