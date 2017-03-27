9f5ebbfad82387ee534b2bf831a722b4a1187648
马三小伙儿 <1790617178@qq.com>
2017-03-27 11:53:42 +0800

    Lua 表

---
+++ LuaTraining/table.lua
@@ -0,0 +1,100 @@
+--最简单的构造函数是{}，用来创建一个空表。可以直接初始化数组
+-- 初始化表
+mytable = {}
+
+-- 指定值
+mytable[1]= "Lua"
+
+-- 移除引用
+mytable = nil
+-- lua 垃圾回收会释放内存
+
+--[[
+当我们为 table a 并设置元素，然后将 a 赋值给 b，则 a 与 b 都指向同一个内存。
+如果 a 设置为 nil ，则 b 同样能访问 table 的元素。如果没有指定的变量指向a
+Lua的垃圾回收机制会清理相对应的内存。
+以下实例演示了以上的描述情况
+]]--
+
+-- 简单的 table
+mytable = {}
+print("mytable 的类型是 ",type(mytable))
+
+mytable[1]= "Lua"
+mytable["wow"] = "修改前"
+print("mytable 索引为 1 的元素是 ", mytable[1])
+print("mytable 索引为 wow 的元素是 ", mytable["wow"])
+
+-- alternatetable和mytable的是指同一个 table
+alternatetable = mytable
+
+print("alternatetable 索引为 1 的元素是 ", alternatetable[1])
+print("mytable 索引为 wow 的元素是 ", alternatetable["wow"])
+
+alternatetable["wow"] = "修改后"
+
+print("mytable 索引为 wow 的元素是 ", mytable["wow"])
+
+-- 释放变量
+alternatetable = nil
+print("alternatetable 是 ", alternatetable)
+
+-- mytable 仍然可以访问
+print("mytable 索引为 wow 的元素是 ", mytable["wow"])
+
+mytable = nil
+print("mytable 是 ", mytable)
+
+--table 连接
+fruits = {"banana","orange","apple"}
+-- 返回 table 连接后的字符串
+print("连接后的字符串 ",table.concat(fruits))
+
+-- 指定连接字符
+print("连接后的字符串 ",table.concat(fruits,", "))
+
+-- 指定索引来连接 table
+print("连接后的字符串 ",table.concat(fruits,", ", 2,3))
+
+
+-- 插入和移除
+fruits = {"banana","orange","apple"}
+
+-- 在末尾插入
+table.insert(fruits,"mango")
+print("索引为 4 的元素为 ",fruits[4])
+
+-- 在索引为 2 的键处插入
+table.insert(fruits,2,"grapes")
+print("索引为 2 的元素为 ",fruits[2])
+
+print("最后一个元素为 ",fruits[5])
+table.remove(fruits)
+print("移除后最后一个元素为 ",fruits[5])
+
+--Table 排序
+fruits = {"banana","orange","apple","grapes"}
+print("排序前")
+for k,v in ipairs(fruits) do
+	print(k,v)
+end
+
+table.sort(fruits)
+print("排序后")
+for k,v in ipairs(fruits) do
+	print(k,v)
+end
+
+-- table.maxn 在 Lua5.2 之后该方法已经不存在了，我们定义了 table_maxn 方法来实现
+function table_maxn(t)
+    local mn = 0
+    for k, v in pairs(t) do
+        if mn < k then
+            mn = k
+        end
+    end
+    return mn
+end
+tbl = {[1] = "a", [2] = "b", [3] = "c", [26] = "z"}
+print("tbl 长度 ", #tbl)
+print("tbl 最大值 ", table_maxn(tbl))
