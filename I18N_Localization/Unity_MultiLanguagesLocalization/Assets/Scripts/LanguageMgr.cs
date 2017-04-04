cba248674a0df3d8d5f6f019eb54c970980d2566
XINCGer <1790617178@qq.com>
2017-04-04 23:03:40 +0800

    Text组件UI赋值

--- I18N_Localization/Unity_MultiLanguagesLocalization/Assets/Scripts/LanguageMgr.cs
+++ I18N_Localization/Unity_MultiLanguagesLocalization/Assets/Scripts/LanguageMgr.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LanguageMgr : MonoBehaviour {
+public class LanguageMgr : MonoBehaviour
+{
 
     /// <summary>
     /// 语言类型
@@ -10,48 +11,55 @@ public class LanguageMgr : MonoBehaviour {
     [SerializeField]
     private SystemLanguage language;
 
-    private LanguageMgr _instance;
+    private static LanguageMgr _instance;
 
     /// <summary>
     /// 相同的key对应不同语言的value
     /// </summary>
     private Dictionary<string, string> dict = new Dictionary<string, string>();
 
-    private void LoadLanguage() {
+    private void LoadLanguage()
+    {
         //加载文件
         TextAsset textAsset = Resources.Load<TextAsset>(language.ToString());
-        if (textAsset == null) {
+        if (textAsset == null)
+        {
             Debug.LogError("没有该语言的本地化文件！");
             return;
         }
         //通过\n分割每行内容
         string[] lines = textAsset.text.Split('\n');
         //获取key,value
-        for (int i = 0; i < lines.Length; i++) {
+        for (int i = 0; i < lines.Length; i++)
+        {
             //检测,如果为空串直接跳过
-            if (string.IsNullOrEmpty(lines[i])) {
+            if (string.IsNullOrEmpty(lines[i]))
+            {
                 continue;
             }
             //获取key,value
             string[] kv = lines[i].Split(':');
             //保存键值对
             dict.Add(kv[0], kv[1]);
-            Debug.Log(kv[0] + ":" + kv[1]);
         }
 
     }
 
-    void Awake() {
+    void Awake()
+    {
         _instance = this;
         LoadLanguage();
     }
 
-    public LanguageMgr GetInstance() {
+    public static LanguageMgr GetInstance()
+    {
         return _instance;
     }
 
-    public string GetText(string key) {
-        if (dict.ContainsKey(key)) {
+    public string GetText(string key)
+    {
+        if (dict.ContainsKey(key))
+        {
             return dict[key];
         }
         return string.Empty;
---
+++ I18N_Localization/Unity_MultiLanguagesLocalization/Assets/Scripts/TextUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private string key;
+    // Use this for initialization
+    void Start()
+    {
+        if (!string.IsNullOrEmpty(key))
+        {
+            string value = LanguageMgr.GetInstance().GetText(key);
+            if (!string.IsNullOrEmpty(value))
+            {
+                GetComponent<Text>().text = value;
+            }
+        }
+    }
+
+}
