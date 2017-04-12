fa768ad29aa908a96ecdcc8f22f17d019d82d430
马三小伙儿 <1790617178@qq.com>
2017-04-12 20:07:45 +0800

    版本回退，文件夹改名

---
+++ NetWorkAndResources/JsonDataDemo/Assets/Scripts/JsonObjectModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Json对象模型
+/// </summary>
+public class JsonObjectModel
+{
+
+    //JsonArrayModel类型的列表
+    public List<JsonArrayModel> infoList;
+
+}
