83c412004c040ca474fc4cdbf9410bc312d7939d
马三小伙儿 <1790617178@qq.com>
2017-04-13 21:05:21 +0800

    Unity3D移动平台动态读取外部文件

---
+++ NetWorkAndResources/MobilePlatformDynamicReadExternalFiles/Assets/Scripts/XMLReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XMLReader : MonoBehaviour
+{
+
+    public Text text;
+    private string result;
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadXML();
+        SetText();
+    }
+    
+    
+    private void LoadXML()
+    {
+        result = Resources.Load<TextAsset>("Test").ToString();
+        XmlDocument xmlReader = new XmlDocument();
+        xmlReader.LoadXml(result);
+    }
+
+    private void SetText()
+    {
+        text.text = result;
+
+    }
+}
