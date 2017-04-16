6135fed180b2374dfc827dd2b7b7dd3e38650103
马三小伙儿 <1790617178@qq.com>
2017-04-16 12:13:53 +0800

    StreamingAsset

---
+++ NetWorkAndResources/MobilePlatformDynamicReadExternalFiles/Assets/Scripts/AssetBunbleReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssetBunbleReader : MonoBehaviour {
+
+    private string _result;
+    public Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadXML();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void LoadXML()
+    {
+        AssetBundle AssetBundleCsv = new AssetBundle();
+        //读取放入StreamingAssets文件夹中的bundle文件
+        string str = Application.streamingAssetsPath + "/" + "TestXML.bundle";
+        WWW www = new WWW(str);
+        www = WWW.LoadFromCacheOrDownload(str, 0);
+        AssetBundleCsv = www.assetBundle;
+
+        string path = "Test";
+
+        TextAsset test = AssetBundleCsv.Load(path, typeof(TextAsset)) as TextAsset;
+
+        _result = test.ToString();
+        text.text = _result;
+    }
+}
---
+++ NetWorkAndResources/MobilePlatformDynamicReadExternalFiles/Assets/Scripts/XMLReaderStreaming.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XMLReaderStreaming : MonoBehaviour
+{
+
+    private string _result;
+    public Text text;
+
+    public void Start()
+    {
+        StartCoroutine(LoadXML());  
+    }
+
+    
+    /// <summary>
+    /// 如前文所述，streamingAssets只能使用www来读取，
+    /// 如果不是使用www来读取的同学，就不要问为啥读不到streamingAssets下的内容了。
+    /// 这里还可以使用persistenDataPath来保存从streamingassets那里读到内容。
+    /// </summary>
+    IEnumerator LoadXML()
+    {
+        string sPath = "file:// "+Application.streamingAssetsPath + "/Test.xml";
+        WWW www = new WWW(sPath);
+        yield return www;
+        _result = www.text;
+        text.text = _result;
+    }
+}
