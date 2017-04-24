735650fb2292b6351741c8cd6d961524a23ce61d
马三小伙儿 <1790617178@qq.com>
2017-04-24 21:37:10 +0800

    利用Json国际化

---
+++ I18N_Localization/I18N_By_Json/Assets/Scripts/LanguageMgr.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class LanguageMgr : MonoBehaviour
+{
+
+    /// <summary>
+    /// 语言类型
+    /// </summary>
+    [SerializeField]
+    private SystemLanguage language;
+
+    private static LanguageMgr _instance;
+
+    /// <summary>
+    /// 相同的key对应不同语言的value
+    /// </summary>
+    private Dictionary<string, string> dict = new Dictionary<string, string>();
+
+    private void LoadLanguage()
+    {
+        //加载文件
+        TextAsset textAsset = Resources.Load<TextAsset>(language.ToString());
+        if (textAsset == null)
+        {
+            Debug.LogError("没有该语言的本地化文件！");
+            return;
+        }
+        //解析Json文件内容
+        JsonData jsonData = JsonMapper.ToObject(textAsset.text);
+
+        foreach (JsonData data in jsonData)
+        {
+            dict.Add(data["key"].ToString(),data["value"].ToString());
+        }
+
+    }
+
+    void Awake()
+    {
+        _instance = this;
+        LoadLanguage();
+    }
+
+    public static LanguageMgr GetInstance()
+    {
+        return _instance;
+    }
+
+    public string GetText(string key)
+    {
+        if (dict.ContainsKey(key))
+        {
+            return dict[key];
+        }
+        return string.Empty;
+    }
+}
+
+
+public class JsonObj
+{
+    //要与Json中的属性名称一一对应
+    public string key;
+    public string value;
+}
---
+++ I18N_Localization/I18N_By_Json/Assets/Scripts/TextUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUI : MonoBehaviour
+{
+    public Text title;
+    public Text startBtnText;
+    public Text continueBtnText;
+    public Text exitBtnText;
+
+    // Use this for initialization
+    void Start()
+    {
+        IsNull(title,"welcome");
+        IsNull(startBtnText, "start");
+        IsNull(continueBtnText, "continue");
+        IsNull(exitBtnText, "exit");
+    }
+
+    /// <summary>
+    /// 判空操作
+    /// </summary>
+    /// <param name="text"></param>text组件
+    /// <param name="key"></param>对应的键值
+    public void IsNull(Text text, string key)
+    {
+        if (null != text)
+        {
+            string value = LanguageMgr.GetInstance().GetText(key);
+            if (!string.IsNullOrEmpty(value))
+            {
+                text.text = value;
+            }
+        }
+    }
+}
