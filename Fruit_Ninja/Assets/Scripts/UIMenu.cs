48969ca9220333d7873f0cac3c94c41bc6b4ec05
XINCGer <1790617178@qq.com>
2017-05-07 23:30:07 +0800

    UI控制脚本

---
+++ Fruit_Ninja/Assets/Scripts/UIMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour
+{
+
+    private Button btnPlay;
+    private Button btnSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitComponents();
+        btnPlay.onClick.AddListener(OnBtnPlayClick);
+        btnSound.onClick.AddListener(OnBtnSoundClick);
+    }
+
+
+    private void InitComponents()
+    {
+        btnPlay = transform.Find("Btn_Play").GetComponent<Button>();
+        btnSound = transform.Find("Btn_Sound").GetComponent<Button>();
+    }
+
+    private void OnBtnPlayClick()
+    {
+        SceneManager.LoadScene("",LoadSceneMode.Single);
+    }
+
+    private void OnBtnSoundClick()
+    {
+        
+    }
+
+    public void OnDestroy()
+    {
+        btnPlay.onClick.RemoveAllListeners();
+        btnSound.onClick.RemoveAllListeners();
+    }
+
+
+}
