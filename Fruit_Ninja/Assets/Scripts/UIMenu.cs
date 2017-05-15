09d246fcff66f1e1c2afa1ff535752658f5efaf1
XINCGer <1790617178@qq.com>
2017-05-15 22:34:30 +0800

    UI控制脚本

--- Fruit_Ninja/Assets/Scripts/UIMenu.cs
+++ Fruit_Ninja/Assets/Scripts/UIMenu.cs
@@ -11,6 +11,11 @@ public class UIMenu : MonoBehaviour
     private Button btnPlay;
     private Button btnSound;
 
+    private AudioSource audioSourceBG;
+    private Image imgSound;
+
+    public Sprite[] spriteSound;
+
     // Use this for initialization
     void Start()
     {
@@ -24,16 +29,27 @@ public class UIMenu : MonoBehaviour
     {
         btnPlay = transform.Find("Btn_Play").GetComponent<Button>();
         btnSound = transform.Find("Btn_Sound").GetComponent<Button>();
+        audioSourceBG = transform.Find("Btn_Sound").GetComponent<AudioSource>();
+        imgSound = transform.Find("Btn_Sound").GetComponent<Image>();
     }
 
     private void OnBtnPlayClick()
     {
-        SceneManager.LoadScene("",LoadSceneMode.Single);
+        SceneManager.LoadScene("Play",LoadSceneMode.Single);
     }
 
     private void OnBtnSoundClick()
     {
-        
+        if (audioSourceBG.isPlaying)
+        {
+            audioSourceBG.Pause();
+            imgSound.sprite = spriteSound[1];
+        }
+        else
+        {
+            audioSourceBG.Play();
+            imgSound.sprite = spriteSound[0];
+        }
     }
 
     public void OnDestroy()
