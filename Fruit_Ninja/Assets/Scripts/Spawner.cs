f56ddaf321451a1fcaf9b7306b95ffc8287989b9
XINCGer <1790617178@qq.com>
2017-05-18 23:18:47 +0800

    创建水果产生脚本

---
+++ Fruit_Ninja/Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] fruits;
+    public GameObject bomb;
+
+    public float spawnTime = 1f;
+    public float countTime = 0f;
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        countTime += Time.deltaTime;
+
+        if (countTime >= spawnTime)
+        {
+            SwapnObject(SpwanType.Apple);
+            countTime = 0f;
+        }
+    }
+
+    private void SwapnObject(SpwanType type)
+    {
+        Debug.Log("Swpan" + Time.time);
+
+    }
+
+
+
+}
+
+public enum SpwanType : byte
+{
+    Apple,
+    Lemon,
+    Watermelon,
+    Bomb
+}
--- Fruit_Ninja/Assets/Scripts/UIMenu.cs
+++ Fruit_Ninja/Assets/Scripts/UIMenu.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
