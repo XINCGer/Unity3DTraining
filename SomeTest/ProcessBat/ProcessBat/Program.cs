93554b8eed85b1335ec28d642654e07e6658bacd
XINCGer <1790617178@qq.com>
2017-05-19 23:36:23 +0800

    C#执行外部exe、bat文件

---
+++ SomeTest/ProcessBat/ProcessBat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessBat
+{
+    /// <summary>
+    /// C#程序执行外部exe、bat文件
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 如果要运行时隐藏dos窗口，需使用下面的代码
+
+            //proc.StartInfo.UseShellExecute = false;
+            //proc.StartInfo.CreateNoWindow = true;
+
+            Process proc = null;
+            try
+            {
+                proc = new Process();
+                proc.StartInfo.FileName = @"./test.bat";
+
+                //proc.StartInfo.Arguments = string.Format("10");
+                //proc.StartInfo.CreateNoWindow = false;
+
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.Start();
+                proc.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Occurred :{0},{1}", ex.Message, ex.StackTrace.ToString());
+            }
+        }
+    }
+}
