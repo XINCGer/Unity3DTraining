3acca40353e56d69135c68f03778a84d170f8021
XINCGer <1790617178@qq.com>
2017-06-03 15:29:50 +0800

    Base

---
+++ PerformanceOptimization/ProfilerExample/Assets/Scripts/BaseProfiler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseProfiler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //FuncWithProfiler();
+        FuncWithoutProfiler();
+    }
+
+    private void FuncWithProfiler()
+    {
+        UnityEngine.Profiling.Profiler.BeginSample("FuncA");
+        FuncA();
+        UnityEngine.Profiling.Profiler.EndSample();
+
+        UnityEngine.Profiling.Profiler.BeginSample("FuncB");
+        FuncB();
+        UnityEngine.Profiling.Profiler.EndSample();
+    }
+
+    private void FuncWithoutProfiler()
+    {
+        FuncA();
+        FuncB();
+    }
+
+    private void FuncA()
+    {
+        int[] arr = new int[10];
+    }
+
+    private void FuncB()
+    {
+
+    }
+}
