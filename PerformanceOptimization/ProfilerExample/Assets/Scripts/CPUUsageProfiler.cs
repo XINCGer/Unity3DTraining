ab2ae0babd4e6469170305a8c084b12bac5cae8a
XINCGer <1790617178@qq.com>
2017-06-03 14:56:07 +0800

    Cpu分析

---
+++ PerformanceOptimization/ProfilerExample/Assets/Scripts/CPUUsageProfiler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPUUsageProfiler : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Func();
+	}
+
+    /// <summary>
+    /// 具体实现
+    /// </summary>
+    private void Func()
+    {
+        UnityEngine.Profiling.Profiler.BeginSample("Func");
+        subFunc();
+        subFunc();
+        UnityEngine.Profiling.Profiler.EndSample();
+    }
+
+    private void subFunc()
+    {
+        UnityEngine.Profiling.Profiler.BeginSample("subFunc");
+        for (int i = 0; i < 100; i++)
+        {
+            int []arr = new int[100];
+        }
+        UnityEngine.Profiling.Profiler.EndSample();
+    }
+}
