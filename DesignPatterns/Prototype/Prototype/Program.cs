9e26f85b007e344e30158959ea4ae5623cfa55eb
XINCGer <1790617178@qq.com>
2017-06-03 22:54:34 +0800

    原型模式基础代码

---
+++ DesignPatterns/Prototype/Prototype/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //原型模式测试1
+            ConcretePrototype1 p1 = new ConcretePrototype1("1");
+            ConcretePrototype1 p2 = (ConcretePrototype1)p1.Clone();
+
+            Console.WriteLine("p1: "+p1.Id);
+            Console.WriteLine("p2: " + p2.Id);
+            //原型模式测试1 END
+
+        }
+    }
+
+}
---
+++ DesignPatterns/Prototype/Prototype/Prototype.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    /// <summary>
+    /// 抽象原型类
+    /// </summary>
+    abstract class Prototype
+    {
+        private string id;
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public Prototype(string id)
+        {
+            this.id = id;
+        }
+
+        //抽象Clone方法
+        public abstract Prototype Clone();
+    }
+
+    /// <summary>
+    /// 具体的原型类
+    /// </summary>
+    class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(string id) : base(id)
+        {
+        }
+
+        public override Prototype Clone()
+        {
+            try
+            {
+                return (Prototype)this.MemberwiseClone();
+            }
+            catch
+            {
+                throw new Exception("Clone Error!");
+            }
+        }
+    }
+}
