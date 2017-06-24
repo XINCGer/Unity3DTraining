f6b246f47251818cfd2fb3afbdd8174292ac0c35
马三小伙儿 <1790617178@qq.com>
2017-06-24 16:30:47 +0800

    以树形结构为例展示组合模式

--- DesignPatterns/CompositePattern/CompositePattern/Leaf.cs
+++ DesignPatterns/CompositePattern/CompositePattern/Leaf.cs
@@ -5,39 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 /// <summary>
-/// Composite类是分支节点的类
+/// Leaf是叶子节点
 /// </summary>
 namespace CompositePattern
 {
-    class Composite : Component
+    class Leaf : Component
     {
-        private List<Component> childList = new List<Component>();
-
-        public Composite(string componentName) : base(componentName)
+        public Leaf(string componentName) : base(componentName)
         {
         }
 
         public override void Add(Component component)
         {
-            childList.Add(component);
+            Console.WriteLine("叶子节点不能添加新的子节点！");
         }
 
         public override void Remove(Component compontent)
         {
-            childList.Remove(compontent);
+            Console.WriteLine("叶子节点不能移除子节点！");
         }
 
         public override void Show(int depth)
         {
-            for(int i = 0; i < depth; i++)
+            for (int i = 0; i < depth; i++)
             {
                 Console.Write("+");
             }
             Console.WriteLine(componentName + "\n");
-            for(int i = 0; i < childList.Count; i++)
-            {
-                childList[i].Show(depth + 2);
-            }
         }
     }
 }
--- DesignPatterns/CompositePattern/CompositePattern/Program.cs
+++ DesignPatterns/CompositePattern/CompositePattern/Program.cs
@@ -13,6 +13,22 @@ namespace CompositePattern
     {
         static void Main(string[] args)
         {
+            //以树形结构为例展示组合模式
+            Composite root = new Composite("树根");
+            root.Add(new Leaf("叶子A"));
+            root.Add(new Leaf("叶子B"));
+
+            Composite composite1 = new Composite("节点1");
+            composite1.Add(new Leaf("1叶子A"));
+            composite1.Add(new Leaf("1叶子B"));
+
+            Composite composite2 = new Composite("节点2");
+            composite2.Add(new Leaf("2叶子A"));
+            composite2.Add(new Leaf("2叶子B"));
+
+            composite1.Add(composite2);
+            root.Add(composite1);
+            root.Show(1);
         }
     }
 }
