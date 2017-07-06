838f646b5c968348da692dfa8a234a70bd615453
XINCGer <1790617178@qq.com>
2017-07-06 23:59:33 +0800

    水果分半

---
+++ Fruit_Ninja/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 物体控制脚本
+/// </summary>
+public class ObjectController : MonoBehaviour {
+
+    public GameObject halfGameObject;
+    private bool isDead = false;
+    public float radio;
+
+    public void OnCut()
+    {
+        if (isDead) return;
+
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject go= Instantiate<GameObject>(halfGameObject);
+            go.GetComponent<Rigidbody>().AddForce(Random.onUnitSphere * radio, ForceMode.Impulse);
+        }
+        isDead = true;
+    }
+}
