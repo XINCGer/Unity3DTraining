5c870ea04e6e2a6d779c8f10392b28ac013ed471
马三小伙儿 <1790617178@qq.com>
2017-07-07 21:48:00 +0800

    以连连看举例

--- DesignPatterns/FlyweightPattern/FlyweightPattern/ImageNode.cs
+++ DesignPatterns/FlyweightPattern/FlyweightPattern/ImageNode.cs
@@ -13,4 +13,25 @@ namespace FlyweightPattern
     {
         public abstract void Show();
     }
+
+    /// <summary>
+    /// 从抽象图片类派生出来图片类
+    /// </summary>
+    class CImageNode : ImageNode
+    {
+        /// <summary>
+        /// 代表图片类型的变量
+        /// </summary>
+        private string imageType;
+
+        public CImageNode(string imageType)
+        {
+            this.imageType = imageType;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("图片是:"+imageType);
+        }
+    }
 }
