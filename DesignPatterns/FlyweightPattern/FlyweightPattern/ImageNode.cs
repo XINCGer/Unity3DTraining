1f2674e720cfaad1d9cbb6070adb118a5ac41aca
马三小伙儿 <1790617178@qq.com>
2017-07-07 21:32:09 +0800

    享元模式

---
+++ DesignPatterns/FlyweightPattern/FlyweightPattern/ImageNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+    /// <summary>
+    /// 抽象图片类
+    /// </summary>
+    public abstract class ImageNode
+    {
+        public abstract void Show();
+    }
+}
