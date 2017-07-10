8f612aa9e5abb772ef5fc0ea1626479725f18362
马三小伙儿 <1790617178@qq.com>
2017-07-10 21:40:16 +0800

    享元模式

---
+++ DesignPatterns/FlyweightPattern/FlyweightPattern/ImageNodeFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+    /// <summary>
+    /// 共享图片工厂类
+    /// </summary>
+    class ImageNodeFactory
+    {
+        private Dictionary<string, CImageNode> imageNodes = new Dictionary<string, CImageNode>();
+
+        public ImageNodeFactory()
+        {
+           imageNodes.Add("Lion",new CImageNode("狮子"));
+           imageNodes.Add("Tiger",new CImageNode("狮子"));
+           imageNodes.Add("Cat",new CImageNode("猫"));
+           imageNodes.Add("Dog",new CImageNode("狗"));
+        }
+
+        public ImageNode GetImage(string imageType)
+        {
+            if (imageNodes.ContainsKey(imageType))
+            {
+                return imageNodes[imageType];
+            }
+            return null;
+        }
+    }
+}
--- DesignPatterns/FlyweightPattern/FlyweightPattern/Program.cs
+++ DesignPatterns/FlyweightPattern/FlyweightPattern/Program.cs
@@ -30,6 +30,61 @@ namespace FlyweightPattern
 
             Flyweight flyF = factory.GetFlyweight("A");
             flyF.Operation(i / 2);
+
+            Console.WriteLine("================================");
+            //共享图片工厂
+            ImageNodeFactory imageNodeFactory = new ImageNodeFactory();
+
+            Dictionary<int,ImageNode> imageList = new Dictionary<int, ImageNode>();
+            int randomNum = 0;
+            Random random= new Random();
+
+            for (int j = 0; j < 10; j++)
+            {
+                randomNum = random.Next(0, 10);
+                switch (randomNum/2)
+                {
+                    case 0:
+                        imageList.Add(j,imageNodeFactory.GetImage("Lion"));
+                        break;
+                    case 1:
+                        imageList.Add(j, imageNodeFactory.GetImage("Tiger"));
+                        break;
+                    case 2:
+                        imageList.Add(j, imageNodeFactory.GetImage("Cat"));
+                        break;
+                    default:
+                        imageList.Add(j, imageNodeFactory.GetImage("Dog"));
+                        break;
+                }
+            }
+
+            //展示所生成的图像
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine(j+"号坐标的");
+                imageList[i].Show();
+            }
+
+            //随机选取两个坐标的图像进行比较
+            int a = 0;
+            int b = 0;
+
+            while (a==b)
+            {
+                a = random.Next(0, 10);
+                b = random.Next(0, 10);
+            }
+
+            //由于使用了共享，所以直接判断对象是否相等即可
+            if (imageList[a] == imageList[b])
+            {
+                Console.WriteLine("可以消除");
+            }
+            else
+            {
+                Console.WriteLine("不可以消除");
+            }
         }
     }
 }
