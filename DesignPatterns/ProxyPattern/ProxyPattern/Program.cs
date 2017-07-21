3013966f462256f749b54f36566a9fd173f9623b
马三小伙儿 <1790617178@qq.com>
2017-07-21 22:02:12 +0800

    代理模式

---
+++ DesignPatterns/ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
