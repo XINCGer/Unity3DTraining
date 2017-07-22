cc8c440e41a75a74452a62ec949914c737c4a371
马三小伙儿 <1790617178@qq.com>
2017-07-22 19:35:39 +0800

    Java文件更新

--- SDK/SDKBase/Android/com.mx.sdkbase/src/com/mx/sdkbase/MainActivity.java
+++ SDK/SDKBase/Android/com.mx.sdkbase/src/com/mx/sdkbase/MainActivity.java
@@ -40,26 +40,55 @@ public class MainActivity extends UnityPlayerActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	
+	/**
+	 * 供Unity调用的求和函数
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public int Sum(int x, int y) {
 		return x + y;
 	}
 
+	/**供Unity调用的比较最大值函数
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public int Max(int x, int y) {
 		return Math.max(x, y);
 	}
 
+	
+	/**供Unity调用的显示吐司的函数
+	 * @param str
+	 */
 	public void MakeToast(String str) {
 		Toast.makeText(this, str, Toast.LENGTH_LONG).show();
 	}
 
+	
+	/**供Unity调用的自加一函数
+	 * @param x
+	 * @return
+	 */
 	public int AddOne(int x) {
 		return x + 1;
 	}
 
+	
+	/**供Unity调用的静态方法，单例类，返回当前的Activity对象
+	 * @return
+	 */
 	public static MainActivity GetInstance() {
 		return instance;
 	}
 	
+	
+	/**供Unity调用的函数，此函数会回调指定的一个Unity中的方法，完成数据的双向交互
+	 * @param str
+	 */
 	public void CallUnityFunc(String str){
 		str=str+"Android Call Unity.";
 		String ReceiveObject="MessageHandler";
