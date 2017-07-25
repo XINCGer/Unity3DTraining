dc7e5d47eeb59b9d57ded2c11472213593da11eb
马三小伙儿 <1790617178@qq.com>
2017-07-25 22:02:23 +0800

    网站举例

---
+++ DesignPatterns/ProxyPattern/ProxyPattern/WebSite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    /// <summary>
+    /// 网站类
+    /// </summary>
+    abstract class WebSite
+    {
+        /// <summary>
+        /// 注册用户名
+        /// </summary>
+        public string userName;
+        /// <summary>
+        /// 注册用户
+        /// </summary>
+        public abstract void Register();
+        /// <summary>
+        /// 登录
+        /// </summary>
+        public abstract void Login();
+        /// <summary>
+        /// 浏览信息
+        /// </summary>
+        public abstract void Browse();
+    }
+}
