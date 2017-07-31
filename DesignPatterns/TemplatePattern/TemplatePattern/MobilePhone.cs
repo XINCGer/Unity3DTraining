3b1fb258c25c7aa5ec6f5401530d56c2c7dfed39
马三小伙儿 <1790617178@qq.com>
2017-07-31 20:37:07 +0800

    模板模式

---
+++ DesignPatterns/TemplatePattern/TemplatePattern/MobilePhone.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    /// <summary>
+    /// 抽象手机类
+    /// </summary>
+    abstract class MobilePhone
+    {
+        public virtual void PowerOn()
+        {
+            Console.WriteLine("手机开机了！");
+        }
+
+        public virtual void PowerOff()
+        {
+            Console.WriteLine("手机关机了！");
+        }
+
+        public virtual void DialUp()
+        {
+            Console.WriteLine("手机拨号！");
+        }
+
+        public virtual void about()
+        {
+            Console.WriteLine("关于手机");
+        }
+    }
+
+
+    class  MobilePhoneA:MobilePhone
+    {
+        public override void about()
+        {
+            base.about();
+            Console.WriteLine("A");
+        }
+    }
+
+    class MobilePhoneB : MobilePhone
+    {
+        public override void about()
+        {
+            base.about();
+            Console.WriteLine("B");
+        }
+    }
+}
---
+++ DesignPatterns/TemplatePattern/TemplatePattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MobilePhoneA mobilePhoneA = new MobilePhoneA();
+            mobilePhoneA.PowerOn();
+            mobilePhoneA.DialUp();
+            mobilePhoneA.about();
+            mobilePhoneA.PowerOff();
+
+            MobilePhoneB mobilePhoneB = new MobilePhoneB();
+            mobilePhoneB.PowerOn();
+            mobilePhoneB.DialUp();
+            mobilePhoneB.about();
+            mobilePhoneB.PowerOff();
+        }
+    }
+}
