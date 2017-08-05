66de5a012b759fc833ee2dddd41ae0e19eb2d9d2
马三小伙儿 <1790617178@qq.com>
2017-08-05 18:31:55 +0800

    状态模式基本代码

---
+++ DesignPatterns/StatePattern/StatePattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new ConcreteStateA());
+
+            context.Request();
+            context.Request();
+            context.Request();
+        }
+    }
+}
---
+++ DesignPatterns/StatePattern/StatePattern/State.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    /// <summary>
+    /// 抽象的状态类
+    /// </summary>
+    abstract class State
+    {
+        public string stateName;
+        public abstract void Handle(Context context);
+    }
+
+
+    class ConcreteStateA : State
+    {
+        public ConcreteStateA()
+        {
+            stateName = "状态A";
+        }
+
+        public override void Handle(Context context)
+        {
+            context.SetState(new ConcreteStateB());
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public ConcreteStateB()
+        {
+            stateName = "状态B";
+        }
+
+        public override void Handle(Context context)
+        {
+            context.SetState(new ConcreteStateA());
+        }
+    }
+
+    class Context
+    {
+        private State state;
+
+        public Context(State state)
+        {
+            this.state = state;
+        }
+
+        public void SetState(State state)
+        {
+            Console.WriteLine("当前状态为："+this.state.stateName);
+            this.state = state;
+            Console.WriteLine("状态改变为："+this.state.stateName);
+            Console.WriteLine("================");
+        }
+
+        public void Request()
+        {
+            state.Handle(this);
+        }
+    }
+}
