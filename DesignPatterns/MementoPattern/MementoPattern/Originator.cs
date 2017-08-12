a8b70a0974652f87847384dcedabd80b3d717ab4
马三小伙儿 <1790617178@qq.com>
2017-08-12 14:47:36 +0800

    备忘录模式

---
+++ DesignPatterns/MementoPattern/MementoPattern/Originator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    class Originator
+    {
+        private string state;
+
+        /// <summary>
+        /// 建立状态存储对象
+        /// </summary>
+        /// <returns></returns>
+        public Memento CreateMemento()
+        {
+            return new Memento(state);
+        }
+
+        /// <summary>
+        /// 设置状态存储对象
+        /// </summary>
+        /// <param name="memento"></param>
+        public void SetMemento(Memento memento)
+        {
+            state = memento.GetState();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("状态为："+state);
+        }
+
+        public string GetState()
+        {
+            return this.state;
+        }
+
+        public void SetState(string state)
+        {
+            this.state = state;
+        }
+    }
+
+
+    /// <summary>
+    /// 备忘录类
+    /// </summary>
+    class Memento
+    {
+        private string state;
+
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+
+        public string GetState()
+        {
+            return this.state;
+        }
+    }
+
+    /// <summary>
+    /// 管理者类
+    /// </summary>
+    class Caretaker
+    {
+        private Memento memento;
+
+        public void SetMemento(Memento memento)
+        {
+            this.memento = memento;
+        }
+
+        public Memento GetMemento()
+        {
+            return this.memento;
+        }
+    }
+}
---
+++ DesignPatterns/MementoPattern/MementoPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Originator originator = new Originator();
+            originator.SetState("开始");
+            originator.Show();
+
+            Caretaker caretaker = new Caretaker();
+            caretaker.SetMemento(originator.CreateMemento());
+
+            originator.SetState("停止");
+            originator.Show();
+
+            originator.SetMemento(caretaker.GetMemento());
+            originator.Show();
+
+
+        }
+    }
+}
