f1fe205e7c9158d8ed466083c4a5a881f1501ccc
马三小伙儿 <1790617178@qq.com>
2017-08-14 21:30:07 +0800

    备忘录模式

---
+++ DesignPatterns/MementoPattern/MementoPattern/GameRole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    /// <summary>
+    /// 游戏角色信息
+    /// </summary>
+    class GameRole
+    {
+        /// <summary>
+        /// 角色等级
+        /// </summary>
+        private int Level;
+        /// <summary>
+        /// 地图坐标
+        /// </summary>
+        private int Coordinate;
+        /// <summary>
+        /// 章节进度
+        /// </summary>
+        private int Chapter;
+
+        private int HP;
+        private int MP;
+
+        public 
+    }
+}
---
+++ DesignPatterns/MementoPattern/MementoPattern/GameState.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    /// <summary>
+    /// 游戏备忘录类
+    /// </summary>
+    class GameState
+    {
+        /// <summary>
+        /// 等级
+        /// </summary>
+        private int _level;
+
+        private int _chapter;
+        private int _coordinate;
+        private int HP;
+        private int MP;
+
+
+        /// <summary>
+        /// 等级
+        /// </summary>
+        public int Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+
+        public int Chapter
+        {
+            get { return _chapter; }
+            set { _chapter = value; }
+        }
+
+        public int Coordinate
+        {
+            get { return _coordinate; }
+            set { _coordinate = value; }
+        }
+
+        public int Hp
+        {
+            get { return HP; }
+            set { HP = value; }
+        }
+
+        public int Mp
+        {
+            get { return MP; }
+            set { MP = value; }
+        }
+    }
+}
