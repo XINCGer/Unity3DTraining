9f7eb028dde251c556291f518de73dd68b16207f
马三小伙儿 <1790617178@qq.com>
2017-08-29 20:51:16 +0800

    访问者模式基本代码

---
+++ DesignPatterns/VisitorPattern/VisitorPattern/Element.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+
+    /// <summary>
+    /// 抽象元素类
+    /// </summary>
+    abstract class Element
+    {
+        /// <summary>
+        /// 抽象的接受访问的方法
+        /// </summary>
+        /// <param name="visitor"></param>
+        public abstract void Accept(Visitor visitor);
+    }
+
+    class ConcreteElementA : Element
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitConcreteElementA(this);
+        }
+
+        /// <summary>
+        /// 操作A
+        /// </summary>
+        public void OperationA()
+        {
+            
+        }
+    }
+
+    class ConcreteElementB : Element
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitConcreteElementB(this);
+        }
+
+        public void OperationB()
+        {
+            
+        }
+    }
+}
---
+++ DesignPatterns/VisitorPattern/VisitorPattern/Visitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    /// <summary>
+    /// 抽象访问者
+    /// </summary>
+    abstract class Visitor
+    {
+        public abstract void VisitConcreteElementA(ConcreteElementA element);
+        public abstract void VisitConcreteElementB(ConcreteElementB element);
+    }
+}
