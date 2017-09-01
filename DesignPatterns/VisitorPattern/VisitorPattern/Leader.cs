d1dbbdd302877a62d51edec3d160dbd0443fd839
马三小伙儿 <1790617178@qq.com>
2017-09-01 10:48:58 +0800

    举例

---
+++ DesignPatterns/VisitorPattern/VisitorPattern/Leader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    /// <summary>
+    /// 抽象的领导类
+    /// </summary>
+    abstract class Leader
+    {
+        public abstract void VisitFinancialReport();
+        public abstract void VisitLogisticsReport();
+    }
+}
---
+++ DesignPatterns/VisitorPattern/VisitorPattern/Reporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    /// <summary>
+    /// 抽象报表
+    /// </summary>
+    abstract class Reporter
+    {
+        /// <summary>
+        /// 抽象的接受访问方法
+        /// </summary>
+        /// <param name="leader"></param>
+        public abstract void Accecpt(Leader leader);
+    }
+}
