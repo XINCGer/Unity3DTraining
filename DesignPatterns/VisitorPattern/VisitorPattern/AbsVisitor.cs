ff3675d59188909088c8c03822eb0bc0092c0130
马三小伙儿 <1790617178@qq.com>
2017-09-02 14:49:50 +0800

    举例说明

---
+++ DesignPatterns/VisitorPattern/VisitorPattern/AbsVisitor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    interface IVisitor
+    {
+        void visit(PartA partA);
+        void visit(PartB partB);
+    }
+
+    class A : IVisitor
+    {
+        public void visit(PartA partA)
+        {
+            Console.WriteLine("A主管"+partA.GetName());
+        }
+
+        public void visit(PartB partB)
+        {
+            //不关心B
+        }
+    }
+
+    class B : IVisitor
+    {
+        public void visit(PartA partA)
+        {
+            //不关心A
+        }
+
+        public void visit(PartB partB)
+        {
+            Console.WriteLine("B主管"+partB.GetName());
+        }
+    }
+
+
+    class CEO : IVisitor
+    {
+        public void visit(PartA partA)
+        {
+            Console.WriteLine("CEO主管"+partA.GetName());
+        }
+
+        public void visit(PartB partB)
+        {
+            Console.WriteLine("CEO主管"+partB.GetName());
+        }
+    }
+}
---
+++ DesignPatterns/VisitorPattern/VisitorPattern/Corporation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    interface IConporation
+    {
+        void Accept(IVisitor v);
+    }
+
+    class PartA : IConporation
+    {
+        private string s = "PartA";
+
+        public void Accept(IVisitor v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            return s;
+        }
+    }
+
+    class PartB:IConporation
+    {
+        private string s = "PartB";
+
+        public void Accept(IVisitor v)
+        {
+            
+        }
+
+        public string GetName()
+        {
+            return s;
+        }
+    }
+
+    class ObjectStructureA
+    {
+        private List<IConporation> list = new List<IConporation>();
+
+        public void AttVisitor(IConporation a)
+        {
+            list.Add(a);
+        }
+
+        public void RemoveVisitor(IConporation a)
+        {
+            list.Remove(a);
+        }
+
+        public void Accept(IVisitor v)
+        {
+            foreach (var i in list)
+            {
+                i.Accept(v);
+            }
+        }
+    }
+}
