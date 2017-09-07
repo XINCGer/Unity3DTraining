6ace437d9923ed194cffb5ca292069fec668dc7d
马三小伙儿 <1790617178@qq.com>
2017-09-07 21:33:02 +0800

    迭代器模式

---
+++ DesignPatterns/IteratorPattern/IteratorPattern/Aggregate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    /// <summary>
+    /// 聚集的抽象
+    /// </summary>
+    abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+    }
+
+    class ConcreteAggregate : Aggregate 
+    {
+        private List<Object> items = new List<object>();
+
+        public override Iterator CreateIterator()
+        {
+            
+        }
+
+        public int Size()
+        {
+            return items.Count;
+        }
+
+        public Object GetItem(int i)
+        {
+            return items[i];
+        }
+
+        public void SetItems(int i,Object obj)
+        {
+            items[i] = obj;
+        }
+    }
+}
---
+++ DesignPatterns/IteratorPattern/IteratorPattern/Iterator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    /// <summary>
+    /// 抽象的迭代器
+    /// </summary>
+    abstract class Iterator
+    {
+        public abstract Object First();
+        public abstract Object Next();
+        public abstract bool IsDone();
+        public abstract Object CurrentItem();
+    }
+
+    class ConcreteIterator : Iterator
+    {
+        private ConcreteAggregate aggregate;
+        private int index = 0;
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this.aggregate = aggregate;
+        }
+        public override object First()
+        {
+            return this.aggregate.GetItem(0);
+        }
+
+        public override object Next()
+        {
+            Object obj = null;
+            index++;
+            if (index < this.aggregate.Size())
+            {
+                obj = this.aggregate.GetItem(index);
+            }
+            return obj;
+        }
+
+        public override bool IsDone()
+        {
+            if (index >= this.aggregate.Size())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override object CurrentItem()
+        {
+            return this.aggregate.GetItem(index);
+        }
+    }
+}
