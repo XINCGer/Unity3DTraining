9a4439004e94a615eeddda5f25d1241cd63051c3
马三小伙儿 <1790617178@qq.com>
2017-09-23 17:43:38 +0800

    C# Socket编程

---
+++ SomeTest/SocketDemo/SocketClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        /// <summary>
+        /// 数据缓冲区
+        /// </summary>
+        private static byte[] dataBuff = new byte[1024];
+
+        private static Socket socketClient;
+        private static int port = 6666;
+
+
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Loopback;
+
+            socketClient = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            try
+            {
+                socketClient.Connect(new IPEndPoint(ip,port));
+                Console.WriteLine("连接服务器成功！");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("连接服务器失败！按回车键退出！");
+                return;
+            }
+
+            //通过clientSocket接受数据
+            int receiveNum = socketClient.Receive(dataBuff);
+            Console.WriteLine("接收服务器消息:{0}",Encoding.UTF8.GetString(dataBuff,0,receiveNum));
+
+            //通过clientSocket发送消息
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                    string message = string.Format("{0}{1}", "Server 你好！", DateTime.Now);
+                    socketClient.Send(Encoding.UTF8.GetBytes(message));
+                    Console.WriteLine("向服务器发送消息:{0}",message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    socketClient.Shutdown(SocketShutdown.Both);
+                    socketClient.Close();
+                    break;
+                }
+            }
+            Console.WriteLine("发送完毕！按回车键退出！");
+            Console.Read();
+        }
+    }
+}
---
+++ SomeTest/SocketDemo/SocketServer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketServer
+{
+    class Program
+    {
+        //缓冲区
+        private static byte[] dataBuff = new byte[1024];
+        //端口号
+        private static int port = 6666;
+        //socket
+        private static Socket socketServer;
+
+        static void Main(string[] args)
+        {
+            //设置监听的IP地址和端口，这里使用回环地址
+            IPAddress ip = IPAddress.Loopback;
+
+            //实例化一个socket对象，确定网络类型，socket类型，协议类型
+            socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //绑定端口与IP
+            socketServer.Bind(new IPEndPoint(ip, port));
+
+            //挂起连接队列的最大长度为15，并启动监听
+            socketServer.Listen(15);
+
+            Console.WriteLine("启动监听{0}成功！", socketServer.LocalEndPoint);
+
+            //创建一个线程用来处理客户端的连接
+            Thread thread = new Thread(ListenClientConnect);
+            thread.Start();
+        }
+
+        /// <summary>
+        /// 处理客户端的连接
+        /// </summary>
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+                //运行到Accept()方法会阻塞程序（同步Socket）
+                //一收到客户端的请求，就新建一个线程来处理数据
+                Socket socketClient = socketServer.Accept();
+                socketClient.Send(Encoding.UTF8.GetBytes("Server:你好！客户端！"));
+                Thread thread = new Thread(ReciveMessage);
+                thread.Start(socketClient);
+            }
+        }
+
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        /// <param name="socketClient"></param>
+        private static void ReciveMessage(object socketClient)
+        {
+            if (null != socketClient)
+            {
+                Socket mySocketClient = socketClient as Socket;
+
+                while (true)
+                {
+                    try
+                    {
+                        //通过socketClient来接收数据
+                        int reciveNum = mySocketClient.Receive(dataBuff);
+                        Console.WriteLine("接收客户端：{0}消息{1}", mySocketClient.RemoteEndPoint, Encoding.UTF8.GetString(dataBuff, 0, reciveNum));
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        mySocketClient.Shutdown(SocketShutdown.Both);
+                        mySocketClient.Close();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
