97c9e6bdf83a29b51ba01db9f0d1b20a240bce00
马三小伙儿 <1790617178@qq.com>
2017-09-25 18:20:39 +0800

    线程测试

---
+++ SomeTest/ThreadDemo/ThreadDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread thread = new Thread(Go);
+            //thread.Start();
+            //thread.Join();
+            //Console.WriteLine("Thread is end!");
+
+            Thread thread = new Thread(()=> { Console.ReadKey(); });
+            if (args.Length > 0)
+            {
+                thread.IsBackground = true;
+            }
+            thread.Start();
+        }
+
+        private static void Go()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("y");
+            }
+        }
+    }
+
+
+}
