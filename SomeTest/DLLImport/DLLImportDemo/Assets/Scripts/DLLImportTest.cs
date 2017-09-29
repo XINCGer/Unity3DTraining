f50b25cead6494b6d72c58701976d623ea0b0534
马三小伙儿 <1790617178@qq.com>
2017-09-29 15:40:41 +0800

    Unity工程

---
+++ SomeTest/DLLImport/DLLImportDemo/Assets/Scripts/DLLImportTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DLLImportTest : MonoBehaviour {
+
+
+    [DllImport("MyDLL")]
+    private static extern int Pow2(int num);
+
+    //[DllImport("MyDLL")]
+    //private static extern void SomeFunction(ref string str);
+
+    // Use this for initialization
+    void Start () {
+		Debug.Log(Pow2(10));
+        //string str = "LiMing.";
+        //SomeFunction(ref str);
+        //Debug.Log(str);
+    }
+
+    
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
