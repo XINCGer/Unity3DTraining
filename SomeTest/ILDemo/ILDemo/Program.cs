c6357f507337965b78c6413854b660408b5960e9
马三小伙儿 <1790617178@qq.com>
2017-10-21 17:22:10 +0800

    阅读IL代码

---
+++ SomeTest/ILDemo/ILDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILDemo
+{
+
+    class Program
+    {
+        public static int n = 0;
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.Func1(Test.n);
+            Console.WriteLine(Test.n);
+            test.Func2(ref Test.n);
+            Console.WriteLine(Test.n);
+
+            Console.WriteLine("============");
+            Func1(n);
+            Console.WriteLine(n);
+            Func2(ref n);
+            Console.WriteLine(n);
+        }
+
+        public static void Func1(int n)
+        {
+            n += 5;
+        }
+
+        public static void Func2(ref int n)
+        {
+            n += 5;
+        }
+    }
+
+    class Test
+    {
+        public static int n = 0;
+        public void Func1(int n)
+        {
+            n += 5;
+        }
+        public void Func2(ref int n)
+        {
+            n += 5;
+        }
+
+    }
+
+}
