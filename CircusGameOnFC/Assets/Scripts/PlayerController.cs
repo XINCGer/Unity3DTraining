42059bf5a6e8e2d49953ec064e83961771d4c1cb
XINCGer <1790617178@qq.com>
2017-10-24 23:56:58 +0800

    主角控制器

---
+++ CircusGameOnFC/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    private bool isGround;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    animator = this.GetComponent<Animator>();
+	    rigidbody2D = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
