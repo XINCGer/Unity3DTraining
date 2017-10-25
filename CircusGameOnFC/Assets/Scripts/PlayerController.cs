724ee672ea8148ccf0112ac7b199a261d47b39e1
XINCGer <1790617178@qq.com>
2017-10-25 23:35:57 +0800

    主角控制器

--- CircusGameOnFC/Assets/Scripts/PlayerController.cs
+++ CircusGameOnFC/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private Rigidbody2D rigidbody2D;
     private bool isGround;
-
+    public BgController bgController;
 	// Use this for initialization
 	void Start ()
 	{
@@ -17,7 +17,44 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
+	    float horizontal = Input.GetAxis("Horizontal");
+	    if (horizontal != 0)
+	    {
+	        if (horizontal > 0)
+	        {
+	            bgController.RollingMap(Direction.Right);
+	        }
+	        else if (horizontal < 0)
+	        {
+	            bgController.RollingMap(Direction.Left);
+	        }
+	        animator.SetBool("IsRun", true);
+	    }
+	    else
+	    {
+	        animator.SetBool("IsRun",false);
+	    }
+
+	    if (Input.GetKeyDown(KeyCode.Space) && isGround)
+	    {
+	        animator.SetBool("IsJump",true);
+            rigidbody2D.AddForce(Vector2.up * 170);
+	    }
 	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        animator.SetBool("IsJump", false);
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        isGround = false;
+    }
+
+
 }
