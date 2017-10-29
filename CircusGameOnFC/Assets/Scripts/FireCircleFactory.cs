a69c8db49a9dde529ef017d05896b3d12e2e2c68
XINCGer <1790617178@qq.com>
2017-10-29 23:24:23 +0800

    火圈工厂

---
+++ CircusGameOnFC/Assets/Scripts/FireCircleFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCircleFactory : MonoBehaviour
+{
+
+    private GameObject fireCirclePrefab;
+    private PlayerController playerController;
+    private float spawnTime = 0f;
+	// Use this for initialization
+	void Start ()
+	{
+	    playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	    if (null == fireCirclePrefab)
+	    {
+	        fireCirclePrefab = Resources.Load<GameObject>("Prefabs/FireCircle");
+	    }
+	    spawnTime = Random.Range(1.5f, 2.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(playerController.Hp<=0)return;
+
+	    spawnTime -= Time.deltaTime;
+	    if (spawnTime <= 0)
+	    {
+            Instantiate(fireCirclePrefab, transform.position, Quaternion.identity);
+            spawnTime = Random.Range(1.5f, 2.5f);
+	    }
+
+	}
+}
