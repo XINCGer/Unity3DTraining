55716f543e875bde1243ba3e235edc5542a2cfdd
马三小伙儿 <1790617178@qq.com>
2017-11-27 14:42:30 +0800

    一些关于Unity编辑器拓展的小测试

---
+++ SomeTest/UnityEditorTest/DebugUILine.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class DebugUILine : MonoBehaviour
+{
+    static Vector3[] fourCorners = new Vector3[4];
+    void OnDrawGizmos()
+    {
+        foreach (MaskableGraphic g in GameObject.FindObjectsOfType<MaskableGraphic>())
+        {
+            if (g.raycastTarget)
+            {
+                RectTransform rectTransform = g.transform as RectTransform;
+                rectTransform.GetWorldCorners(fourCorners);
+                Gizmos.color = Color.blue;
+                for (int i = 0; i < 4; i++)
+                    Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+            }
+        }
+    }
+}
+#endif
---
+++ SomeTest/UnityEditorTest/MenuOptionOverwrite.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MenuOptionOverwrite
+{
+
+    [MenuItem("GameObject/UI/Image")]
+    static void CreateImage()
+    {
+        if (Selection.activeTransform)
+        {
+            if (Selection.activeTransform.GetComponentInParent<Canvas>())
+            {
+                GameObject go = new GameObject("image", typeof(Image));
+                go.GetComponent<Image>().raycastTarget = false;
+                go.transform.SetParent(Selection.activeTransform);
+            }
+        }
+    }
+
+    [MenuItem("GameObject/UI/Text")]
+    static void CreateText()
+    {
+        if (Selection.activeTransform)
+        {
+            if (Selection.activeTransform.GetComponentInParent<Canvas>())
+            {
+                GameObject go = new GameObject("text", typeof(Text));
+                go.GetComponent<Text>().raycastTarget = false;
+                go.transform.SetParent(Selection.activeTransform);
+            }
+        }
+    }
+
+}
