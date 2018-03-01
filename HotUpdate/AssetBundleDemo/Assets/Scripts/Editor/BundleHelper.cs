4424b4a0393f9e820a052771cb91e1d48eba3714
马三小伙儿 <1790617178@qq.com>
2018-03-01 20:58:38 +0800

    打bundle编辑器脚本

---
+++ HotUpdate/AssetBundleDemo/Assets/Scripts/Editor/BundleHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 打bundle助手
+/// </summary>
+public class BundleHelper
+{
+    /// <summary>
+    /// 给所有的资源打bundle
+    /// </summary>
+    [MenuItem("Assets/Build All_Bundles")]
+    private static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
+        AssetDatabase.Refresh();
+    }
+}
