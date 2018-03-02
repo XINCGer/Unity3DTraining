5e8cdfa770c8bf0892919b4338f856a8ecedf34f
马三小伙儿 <1790617178@qq.com>
2018-03-02 19:52:24 +0800

    使用脚本给资源自动设置bundle标签

--- HotUpdate/AssetBundleDemo/Assets/Scripts/Editor/BundleHelper.cs
+++ HotUpdate/AssetBundleDemo/Assets/Scripts/Editor/BundleHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,38 +10,45 @@ using UnityEngine;
 public class BundleHelper
 {
     /// <summary>
-    /// 资源路径-bundle名字映射
+    /// 需要打bundle的资源路径
     /// </summary>
-    private static readonly Dictionary<string, string> path4bundle = new Dictionary<string, string>()
+    private static readonly List<string> BundlePathList = new List<string>()
     {
-        {"Assets/Prefabs","Prefabs" },
-        {"Assets/Scenes","Scenes" },
-        {"Assets/Textures","Textures" },
+        "/Prefabs",
+        "/Scenes",
+        "/Textures",
     };
 
     /// <summary>
-    /// 给所有的资源打bundle
+    /// bundle的输出路径
     /// </summary>
-    [MenuItem("Assets/Build All_Bundles")]
-    private static void BuildAllAssetBundles()
+    private static readonly string abOutputPath = Application.streamingAssetsPath;
+
+    /// <summary>
+    /// 资源的存放目录
+    /// </summary>
+    private static readonly string assetDir = Application.dataPath;
+
+    /// <summary>
+    /// 给所有的资源打bundle,手动设置bundleName标签
+    /// </summary>
+    [MenuItem("Assets/Build All_BundlesManual")]
+    private static void BuildAllAssetBundlesManual()
     {
-        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
+        BuildPipeline.BuildAssetBundles(abOutputPath, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
         AssetDatabase.Refresh();
     }
 
     /// <summary>
     /// 使用脚本给资源自动设置bundle标签
     /// </summary>
-    [MenuItem("Assets/SetBundleNameByDir")]
-    private static void SetBundleNameByDir()
+    [MenuItem("Assets/SetBundleNameAuto")]
+    private static void SetBundleNameAuto()
     {
-        using (var enumator = path4bundle.GetEnumerator())
+        ClearAssetBundlesName();
+        for (int i = 0; i < BundlePathList.Count; i++)
         {
-            while (enumator.MoveNext())
-            {
-                AssetImporter assetImporter = AssetImporter.GetAtPath(enumator.Current.Key);
-                assetImporter.assetBundleName = enumator.Current.Value;
-            }
+            SetAssetBundleNameByPath(assetDir + BundlePathList[i]);
         }
     }
 
@@ -57,5 +65,47 @@ public class BundleHelper
         {
             AssetDatabase.RemoveAssetBundleName(abNames[i], true);
         }
+    }
+
+    /// <summary>
+    /// 设置所有在指定路径下的AssetBundleName 
+    /// </summary>
+    /// <param name="assetPath"></param>
+    private static void SetAssetBundleNameByPath(string assetPath)
+    {
+        //先获取指定路径下的所有Asset，包括子文件夹下的资源 
+        DirectoryInfo dirInfo = new DirectoryInfo(assetPath);
+        //GetFileSystemInfos方法可以获取到指定目录下的所有文件以及子文件夹  
+        FileSystemInfo[] fileInfos = dirInfo.GetFileSystemInfos();
+
+        for (int i = 0; i < fileInfos.Length; i++)
+        {
+            //如果是文件夹则递归处理 
+            if (fileInfos[i] is DirectoryInfo)
+            {
+                SetAssetBundleNameByPath(fileInfos[i].FullName);
+            }
+            else if (!fileInfos[i].Name.EndsWith(".meta"))//如果是文件的话，则设置AssetBundleName，并排除掉.meta文件  
+            {
+                //逐个设置AssetBundleName  
+                SetABName(fileInfos[i].FullName);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 设置单独的某个AssetBundle的Name
+    /// </summary>
+    /// <param name="assetPath"></param>
+    private static void SetABName(string assetPath)
+    {
+        string subDir = assetPath.Substring(assetDir.Length);
+        string importPath = string.Format("Assets{0}", subDir);
+        //上一步的GetFileSystemInfos操作,会把路径都改成\\这种格式的Win路径，因此下面要查找 @"\"
+        string tmpName = subDir.Substring(subDir.LastIndexOf(@"\") + 1);
+        string bundleName = tmpName.Remove(tmpName.LastIndexOf("."));
+        AssetImporter assetImporter = AssetImporter.GetAtPath(importPath);
+        assetImporter.assetBundleName = bundleName;
+
     }
 }
