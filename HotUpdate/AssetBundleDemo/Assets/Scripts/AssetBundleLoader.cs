be04f1340828caa6648c4d3db6009166b0b29c98
马三小伙儿 <1790617178@qq.com>
2018-03-05 21:05:58 +0800

    AssetBundle同步/异步加载

--- HotUpdate/AssetBundleDemo/Assets/Scripts/AssetBundleLoader.cs
+++ HotUpdate/AssetBundleDemo/Assets/Scripts/AssetBundleLoader.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssetBundleLoader : MonoBehaviour {
+public class AssetBundleLoader : MonoBehaviour
+{
+    /// <summary>
+    /// 异步通过路径加载Bundle
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="callback"></param>
+    public void LoadAssetBundleAsync(string path, Action<AssetBundle> callback)
+    {
+        StartCoroutine(LoadAsyncCoroutine(path, callback));
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// 从文件加载bundle的携程
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    private IEnumerator LoadAsyncCoroutine(string path, Action<AssetBundle> callback)
+    {
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(path);
+        yield return request;
+        callback(request.assetBundle);
+    }
 }
--- HotUpdate/AssetBundleDemo/Assets/Scripts/AssetBundleMgr.cs
+++ HotUpdate/AssetBundleDemo/Assets/Scripts/AssetBundleMgr.cs
@@ -1,16 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class AssetBundleMgr : MonoBehaviour {
+/// <summary>
+/// AssetBundle管理器
+/// </summary>
+public class AssetBundleMgr
+{
+    private static readonly string streamingAssetPath = Application.streamingAssetsPath;
+    private static readonly string persistentDataPath = Application.persistentDataPath;
+    private static AssetBundleMgr instance;
+    private static AssetBundleLoader assetBundleLoader;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private AssetBundleMgr()
+    {
+        if (null == assetBundleLoader)
+        {
+            GameObject go = new GameObject("AssetBundleLoader");
+            GameObject.DontDestroyOnLoad(go);
+            assetBundleLoader = go.AddComponent<AssetBundleLoader>();
+        }
+    }
+
+    public static AssetBundleMgr GetInstance()
+    {
+        if (null == instance)
+        {
+            instance = new AssetBundleMgr();
+        }
+        return instance;
+    }
+
+    /// <summary>
+    /// 从StreamingAssetPath加载(同步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <returns></returns>
+    public static AssetBundle LoadFromStreamingAssetPath(string bundleName)
+    {
+        return AssetBundle.LoadFromFile(Path.Combine(streamingAssetPath, bundleName));
+    }
+
+    /// <summary>
+    /// 从PersistantDataPath加载(同步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <returns></returns>
+    public static AssetBundle LoadFromPersistantDataPath(string bundleName)
+    {
+        return AssetBundle.LoadFromFile(Path.Combine(persistentDataPath, bundleName));
+    }
+
+    /// <summary>
+    /// 从StreamingAssetPath加载(异步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <returns></returns>
+    public static void LoadFromStreamingAssetPathAsync(string bundleName, Action<AssetBundle> callback)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(Path.Combine(streamingAssetPath, bundleName), callback);
+    }
+
+    /// <summary>
+    /// 从PersistantDataPath加载(异步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <param name="callback"></param>
+    public static void LoadFromPersistantDataPathAsync(string bundleName, Action<AssetBundle> callback)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(Path.Combine(persistentDataPath, bundleName), callback);
+    }
 }
