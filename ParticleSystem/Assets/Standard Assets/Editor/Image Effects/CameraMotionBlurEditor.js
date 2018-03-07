08ea560c6403180229e5a3c21da3e5a00d5c510a
马三小伙儿 <1790617178@qq.com>
2018-03-07 23:10:10 +0800

    修复报错

--- ParticleSystem/Assets/Standard Assets/Editor/Image Effects/CameraMotionBlurEditor.js
+++ ParticleSystem/Assets/Standard Assets/Editor/Image Effects/CameraMotionBlurEditor.js
@@ -7,7 +7,7 @@ class CameraMotionBlurEditor extends Editor
 	var serObj : SerializedObject;	
 		
   var filterType : SerializedProperty;
-  var preview : SerializedProperty;
+  var preview_ : SerializedProperty;
   var previewScale : SerializedProperty;
   var movementScale : SerializedProperty;
   var jitter : SerializedProperty;
@@ -28,7 +28,7 @@ class CameraMotionBlurEditor extends Editor
 		
     filterType = serObj.FindProperty ("filterType");
 
-    preview = serObj.FindProperty ("preview");
+    preview_ = serObj.FindProperty ("preview");
     previewScale = serObj.FindProperty ("previewScale");
 
     movementScale = serObj.FindProperty ("movementScale");
@@ -89,8 +89,8 @@ class CameraMotionBlurEditor extends Editor
 
     EditorGUILayout.Separator ();
 
-    EditorGUILayout.PropertyField (preview, new GUIContent("Preview"));
-    if (preview.boolValue)
+    EditorGUILayout.PropertyField (preview_, new GUIContent("Preview"));
+    if (preview_.boolValue)
       EditorGUILayout.PropertyField (previewScale, new GUIContent(""));    
         	
     serObj.ApplyModifiedProperties();
