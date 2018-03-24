5a60ce1df9833877dfe416cc3b554bbfdaf1f394
马三小伙儿 <1790617178@qq.com>
2018-03-24 15:50:10 +0800

    Excel表转lua工具

---
+++ XlsxTools/Xls2Lua/Xls2Lua/FileExporter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xls2Lua
+{
+    /// <summary>
+    /// 负责最终文件的输出保存等操作类
+    /// </summary>
+    class FileExporter
+    {
+        private string intDir = "";
+        private string outDir = "";
+    }
+}
