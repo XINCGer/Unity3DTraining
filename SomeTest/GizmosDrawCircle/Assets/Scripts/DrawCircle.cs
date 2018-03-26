f6f91f988d3efa0894ba115dd2e09ed0058fd64b
马三小伙儿 <1790617178@qq.com>
2018-03-26 15:07:43 +0800

    Gizmos绘制圆形

---
+++ SomeTest/GizmosDrawCircle/Assets/Scripts/DrawCircle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircle : MonoBehaviour
+{
+    public Transform m_Transform;
+    public float m_Radius = 1; // 圆环的半径
+    public float m_Theta = 0.1f; // 值越低圆环越平滑
+    public Color m_Color = Color.green; // 线框颜色
+
+
+    void Start()
+    {
+        if (null == m_Transform) m_Transform = transform;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (m_Transform == null) return;
+        if (m_Theta < 0.0001f) m_Theta = 0.0001f;
+
+        // 设置矩阵
+        Matrix4x4 defaultMatrix = Gizmos.matrix;
+        Gizmos.matrix = m_Transform.localToWorldMatrix;
+
+        // 设置颜色
+        Color defaultColor = Gizmos.color;
+        Gizmos.color = m_Color;
+
+        // 绘制圆环
+        Vector3 beginPoint = Vector3.zero;
+        Vector3 firstPoint = Vector3.zero;
+        for (float theta = 0; theta < 2 * Mathf.PI; theta += m_Theta)
+        {
+            float x = m_Radius * Mathf.Cos(theta);
+            float z = m_Radius * Mathf.Sin(theta);
+            Vector3 endPoint = new Vector3(x, 0, z);
+            if (theta == 0)
+            {
+                firstPoint = endPoint;
+            }
+            else
+            {
+                Gizmos.DrawLine(beginPoint, endPoint);
+            }
+            beginPoint = endPoint;
+        }
+
+        // 绘制最后一条线段
+        Gizmos.DrawLine(firstPoint, beginPoint);
+
+        // 恢复默认颜色
+        Gizmos.color = defaultColor;
+
+        // 恢复默认矩阵
+        Gizmos.matrix = defaultMatrix;
+    }
+}
