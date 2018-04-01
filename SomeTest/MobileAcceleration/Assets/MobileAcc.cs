63627e792a45c436c9cba70f08e1c620e4f99a3e
马三小伙儿 <1790617178@qq.com>
2018-04-01 15:55:24 +0800

    重力加速剂测试

---
+++ SomeTest/MobileAcceleration/Assets/MobileAcc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MobileAcc : MonoBehaviour
+{
+	void OnGUI()
+	{  
+		GUI.Box(new Rect(5, 5, 100, 50), String.Format("{0:0.00}", Input.acceleration.x));
+		GUI.Box(new Rect(5, 60, 100, 50), String.Format("{0:0.00}", Input.acceleration.y));
+		GUI.Box(new Rect(5, 115, 100, 50), String.Format("{0:0.00}", Input.acceleration.z));
+	}
+}
+
