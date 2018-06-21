98b483fd9a08c57d4bb23351a638c8bb9d227eb2
马三小伙儿 <1790617178@qq.com>
2018-06-21 20:57:46 +0800

    重命名为RepeateWindow

---
+++ UnityEditorExtension/MultiEditorWindow/Assets/Editor/RepeateWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeateWindow : EditorWindowBase {
+
+    public static void Popup()
+    {
+        RepeateWindow window = new RepeateWindow();
+        window.Show();
+    }
+}
