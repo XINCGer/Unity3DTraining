b20782bb140a5e16ff769541cc300cbe4935e02e
马三小伙儿 <1790617178@qq.com>
2018-08-06 14:00:51 +0800

    补充DrawCall测试

--- SomeTest/DrawCallTest/Assets/Scripts/DrawcallTest1.cs
+++ SomeTest/DrawCallTest/Assets/Scripts/DrawcallTest1.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class DrawcallTest1 : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    StartCoroutine(Init());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private IEnumerator Init()
+    {
+        yield return null;
+        GameObject prefab = Resources.Load<GameObject>("Cube");
+        //动态合批
+        for (int i = 0; i < 500; i++)
+        {
+            GameObject.Instantiate(prefab, Random.insideUnitSphere * 3, Quaternion.identity);
+        }
+    }
 }
---
+++ SomeTest/DrawCallTest/Assets/Scripts/DrawcallTest2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawcallTest2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Init());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator Init()
+    {
+        yield return null;
+        GameObject prefab = Resources.Load<GameObject>("Cube");
+        //动态合批
+        for (int i = 0; i < 500; i++)
+        {
+            GameObject cube = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            if (i < 100)
+            {
+                cube.transform.localScale = new Vector3(2 + i, 2 + i, 2 + i);
+            }
+        }
+    }
+}
