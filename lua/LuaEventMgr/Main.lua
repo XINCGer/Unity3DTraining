3422169caaf6049918bb448988e9eda575629c84
马三小伙儿 <1790617178@qq.com>
2018-08-11 18:10:58 +0800

    EventMgr事件派发测试

--- lua/LuaEventMgr/Main.lua
+++ lua/LuaEventMgr/Main.lua
@@ -1,4 +1,15 @@
 require("EventMgr")
 
+local function TestCallback_1()
+    print("Callback_1")
+end
+
+local function TestCallback_2()
+    print("Callback_2")
+end
+
 local EventMgr = EventMgr:Instance()
-EventMgr:Test()
+EventMgr:RegisterEvent(1, 1, TestCallback_1)
+EventMgr:RegisterEvent(2, 1, TestCallback_2)
+EventMgr:DispatchEvent(1, 1)
+EventMgr:DispatchEvent(2, 1)
