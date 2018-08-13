6691d14af27b67918ddc66a5485e34930dfec3a5
马三小伙儿 <1790617178@qq.com>
2018-08-13 23:41:49 +0800

    支持派发事件的时候带参数测试

--- lua/LuaEventMgr/Main.lua
+++ lua/LuaEventMgr/Main.lua
@@ -4,12 +4,14 @@ local function TestCallback_1()
     print("Callback_1")
 end
 
-local function TestCallback_2()
+local function TestCallback_2(param)
     print("Callback_2")
+    print(param.id)
+    print(param.pwd)
 end
 
 local EventMgr = EventMgr:Instance()
 EventMgr:RegisterEvent(1, 1, TestCallback_1)
 EventMgr:RegisterEvent(2, 1, TestCallback_2)
 EventMgr:DispatchEvent(1, 1)
-EventMgr:DispatchEvent(2, 1)
+EventMgr:DispatchEvent(2, 1, { id = "abc", pwd = "123" })
