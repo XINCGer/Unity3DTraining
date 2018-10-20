65fe78f2c4e23f2ac9da5234d3c1bca8f8332110
马三小伙儿 <1790617178@qq.com>
2018-10-20 23:35:40 +0800

    ColaPlus插件完善

--- SomeTest/ColaPlus/ColaPlus.lua
+++ SomeTest/ColaPlus/ColaPlus.lua
@@ -3,3 +3,31 @@
 ]]
 
 local ColaPlus = {}
+
+----------------------------------------
+--
+-- external functions
+--
+----------------------------------------
+
+local rawget = rawget
+local type = type
+local pairs = pairs
+local setmetatable = setmetatable
+local getmetatable = getmetatable
+local tostring = tostring
+local error = error
+local select = select
+local assert = assert
+local require = require
+local pcall = pcall
+local debug = debug
+
+-- debug
+local _G = _G
+--~ local _error = error
+--~ local function error (what)
+--~ 	return _error(what, 2)
+--~ end
+
+local _ENV = nil	-- help to do spelling checking, compatible with lua 5.1
