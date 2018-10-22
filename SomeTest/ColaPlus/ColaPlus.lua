9261835113e937163b71a5c3ab225c67a0ef60bb
马三小伙儿 <1790617178@qq.com>
2018-10-22 23:24:44 +0800

    Utilities part 1

--- SomeTest/ColaPlus/ColaPlus.lua
+++ SomeTest/ColaPlus/ColaPlus.lua
@@ -31,3 +31,44 @@ local _G = _G
 --~ end
 
 local _ENV = nil	-- help to do spelling checking, compatible with lua 5.1
+
+----------------------------------------
+--
+-- Utilities part 1
+--
+----------------------------------------
+
+local function createProxy (metatable)
+	return setmetatable({}, metatable)
+end
+
+local function shallowCopy (destTable, sourceTable)
+	for k, v in pairs(sourceTable) do
+		destTable[k] = v
+	end
+end
+
+local function argError (iArg, funcName, needs, gets, errLevel)
+	error(("bad argument #%d to '%s' (%s expected, got %s)")
+		:format(iArg, funcName, needs, gets), errLevel + 1)
+end
+
+local function argListError (iArg, memberName, needs, gets, errLevel)
+	if gets == nil then
+		error(([[bad argument #%d to argument list of %s (%s)]])
+			:format(iArg, memberName, needs), errLevel + 1)
+	else
+		error(([[bad argument #%d to argument list of %s (%s expected, got %s)]])
+			:format(iArg, memberName, needs, gets), errLevel + 1)
+	end
+end
+
+local function initValueError (fieldName, needs, gets, errLevel)
+	if gets == nil then
+		error(([[bad inital value to field '%s' (%s)]])
+			:format(fieldName, needs), errLevel + 1)
+	else
+		error(([[bad inital value to field '%s' (%s expected, got %s)]])
+			:format(fieldName, needs, gets), errLevel + 1)
+	end
+end
