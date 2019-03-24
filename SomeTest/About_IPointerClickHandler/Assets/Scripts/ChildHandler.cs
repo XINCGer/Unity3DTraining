3a5f26f5f804c6f6315ca875039c647e4db630cf
马三小伙儿 <1790617178@qq.com>
2019-03-24 19:05:44 +0800

    IPointerClickHandler

---
+++ SomeTest/About_IPointerClickHandler/Assets/Scripts/ChildHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChildHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Child OnPointerClick" + eventData.ToString());
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Child OnPointerDown" + eventData.ToString());
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Child OnPointerUp" + eventData.ToString());
+    }
+}
---
+++ SomeTest/About_IPointerClickHandler/Assets/Scripts/ParentHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ParentHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Parent OnPointerClick" + eventData.ToString());
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Parent OnPointerDown" + eventData.ToString());
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Parent OnPointerUp" + eventData.ToString());
+    }
+}
