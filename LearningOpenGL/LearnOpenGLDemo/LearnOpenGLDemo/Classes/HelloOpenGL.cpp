2bb813852be41cb2d802981e879bab580fabb8b3
马三小伙儿 <1790617178@qq.com>
2019-04-26 21:30:29 +0800

    测速代码

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/HelloOpenGL.cpp
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/HelloOpenGL.cpp
@@ -1,5 +1,10 @@
 #include<glad/glad.h>
+#include<GLFW/glfw3.h>
 
 int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	return 0;
 }
