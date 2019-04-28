b06e0efd946d6d830dc31e4a770cbdfe8fda96dc
马三小伙儿 <1790617178@qq.com>
2019-04-28 20:54:13 +0800

    添加测试代码

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/HelloOpenGL.cpp
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/HelloOpenGL.cpp
@@ -1,10 +1,19 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<iostream>
 
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow * window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (NULL == window) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 	return 0;
 }
