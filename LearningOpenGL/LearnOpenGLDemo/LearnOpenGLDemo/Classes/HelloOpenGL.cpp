9e05916b5bb44caa335ff2a391f6546420a62fa9
马三小伙儿 <1790617178@qq.com>
2019-04-29 20:53:37 +0800

    OpenGL清屏

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/HelloOpenGL.cpp
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/HelloOpenGL.cpp
@@ -28,7 +28,8 @@ int main() {
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
-
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
