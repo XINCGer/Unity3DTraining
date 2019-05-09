a5f545e3fc0afd2db6b4d433ff696d1844af307c
马三小伙儿 <1790617178@qq.com>
2019-05-09 22:39:36 +0800

    准备使用out 修改颜色值

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/1_gettting_started/shader_uniform.cpp
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/1_gettting_started/shader_uniform.cpp
@@ -8,17 +8,21 @@ void processInput(GLFWwindow *window);
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 outColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos, 1.0);\n"
+"   outColor = aColor;\n"
+
 "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 outColor;\n"
+"int vec3 outColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = outColor;\n"
+"   FragColor = vec4(outColor,1.0f);\n"
 "}\n\0";
 
 int main() {
