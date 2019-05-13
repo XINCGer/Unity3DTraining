b50b6395402319581470dee466d43c8add2a337e
马三小伙儿 <1790617178@qq.com>
2019-05-13 21:15:56 +0800

    完善shader_s头文件

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/shader_s.h
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/shader_s.h
@@ -13,7 +13,49 @@ public:
 	unsigned int ID;
 
 	//构造器，读取并构建着色器
-	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath) {
+
+		//1.从文件读取vertex/fragment source code
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+
+		//确保ifstream对象可以抛出异常
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try
+		{
+			//打开文件
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+			//读取文件内容到流中
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+			//关闭文件句柄
+			vShaderFile.close();
+			fShaderFile.close();
+			//流转为string
+			vertexCode = vShaderStream.str();
+			fragmentCode = vShaderStream.str();
+		}
+		catch (const std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		}
+
+		const char* vShaderCode = vertexCode.c_str();
+		const char* fShaderCode = fragmentCode.c_str();
+
+		//编译shader
+		unsigned int vertex, fragment;
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vShaderCode, NULL);
+		glCompileShader(vertex);
+		
+	}
 
 	//使用/激活程序
 	void use();
@@ -22,6 +64,28 @@ public:
 	void setBool(const std::string &name, bool value) const;
 	void setInt(const std::string &name, int value) const;
 	void setFloat(const std::string &name, float value) const;
+
+private:
+	//检测Shader编译、链接错误的工具方法
+	void checkComplieErrors(unsigned int shader, std::string type) {
+		int success;
+		char infoLog[1024];
+		if ("PROGRAM" != type) {
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+			if (!success) {
+				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+		}
+		else {
+			glGetProgramiv(shader, GL_LINK_STATUS, &success);
+			if (!success) {
+				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+
+		}
+	}
 };
 #endif // !SHADER_H
 
