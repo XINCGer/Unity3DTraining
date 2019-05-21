1ebccd327e64fcbe58ce1088c1688811596659c8
马三小伙儿 <1790617178@qq.com>
2019-05-21 20:41:36 +0800

    添加Shaders

---
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Shaders/4.1.texture.fs
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+
+void main()
+{
+	FragColor = texture(texture1, TexCoord);
+}
