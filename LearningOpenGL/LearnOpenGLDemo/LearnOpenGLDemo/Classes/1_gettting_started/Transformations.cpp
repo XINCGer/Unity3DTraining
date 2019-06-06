ecd27a310ff07763a2dad95f88c6b2d8793f6a0b
马三小伙儿 <1790617178@qq.com>
2019-06-06 18:13:27 +0800

    将箱子摆放在左上角并不断地缩放

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/1_gettting_started/Transformations.cpp
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Classes/1_gettting_started/Transformations.cpp
@@ -136,8 +136,9 @@ int main() {
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		glm::mat4 trans = glm::mat4(1.0f);
-		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+		trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
+		float scaleRatio = sin(glfwGetTime()) / 2 + 0.5f;
+		trans = glm::scale(trans, glm::vec3(scaleRatio, scaleRatio, scaleRatio));
 		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
