bf8182446b38897f0413dfffbba5e0923e6eb961
马三小伙儿 <1790617178@qq.com>
2019-06-10 20:55:52 +0800

    完善Shader类

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/shader_s.h
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/shader_s.h
@@ -1,7 +1,9 @@
 #ifndef SHADER_H
 #define SHADER_H
 #include <glad/glad.h>  // 包含glad来获取所有的必须OpenGL头文件
-
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -86,6 +88,48 @@ public:
 	}
 	void setFloat(const std::string &name, float value) const {
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	}
+
+	void setVec2(const std::string &name, const glm::vec2 &value) const
+	{
+		glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+	void setVec2(const std::string &name, float x, float y) const
+	{
+		glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+	}
+
+	void setVec3(const std::string &name, const glm::vec3 &value) const
+	{
+		glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+	void setVec3(const std::string &name, float x, float y, float z) const
+	{
+		glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+	}
+
+	void setVec4(const std::string &name, const glm::vec4 &value) const
+	{
+		glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+	void setVec4(const std::string &name, float x, float y, float z, float w) const
+	{
+		glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+	}
+
+	void setMat2(const std::string &name, const glm::mat2 &mat) const
+	{
+		glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
+	void setMat3(const std::string &name, const glm::mat3 &mat) const
+	{
+		glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
+	void setMat4(const std::string &name, const glm::mat4 &mat) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
 
 private:
