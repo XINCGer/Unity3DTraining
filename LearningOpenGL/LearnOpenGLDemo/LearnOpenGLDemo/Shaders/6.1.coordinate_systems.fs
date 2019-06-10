442be53e0a84c4fa149120462d2fa4cd145a0892
马三小伙儿 <1790617178@qq.com>
2019-06-10 20:56:10 +0800

    添加Shader文件

---
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/Shaders/6.1.coordinate_systems.fs
@@ -0,0 +1,12 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+}
