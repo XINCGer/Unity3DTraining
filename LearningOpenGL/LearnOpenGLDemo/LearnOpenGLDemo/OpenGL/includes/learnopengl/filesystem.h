a0eec924b07b8062d75946a23f7e920aa4ad7a96
马三小伙儿 <1790617178@qq.com>
2019-06-23 17:27:31 +0800

    格式化代码

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/filesystem.h
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/filesystem.h
@@ -7,41 +7,41 @@
 class FileSystem
 {
 private:
-  typedef std::string (*Builder) (const std::string& path);
+	typedef std::string(*Builder) (const std::string& path);
 
 public:
-  static std::string getPath(const std::string& path)
-  {
-    static std::string(*pathBuilder)(std::string const &) = getPathBuilder();
-    return (*pathBuilder)(path);
-  }
+	static std::string getPath(const std::string& path)
+	{
+		static std::string(*pathBuilder)(std::string const &) = getPathBuilder();
+		return (*pathBuilder)(path);
+	}
 
 private:
-  static std::string const & getRoot()
-  {
-    static char const * givenRoot = getenv("LOGL_ROOT_PATH");
-    static std::string root = (givenRoot != nullptr ? givenRoot : "");
-    return root;
-  }
+	static std::string const & getRoot()
+	{
+		static char const * givenRoot = getenv("LOGL_ROOT_PATH");
+		static std::string root = (givenRoot != nullptr ? givenRoot : "");
+		return root;
+	}
 
-  //static std::string(*foo (std::string const &)) getPathBuilder()
-  static Builder getPathBuilder()
-  {
-    if (getRoot() != "")
-      return &FileSystem::getPathRelativeRoot;
-    else
-      return &FileSystem::getPathRelativeBinary;
-  }
+	//static std::string(*foo (std::string const &)) getPathBuilder()
+	static Builder getPathBuilder()
+	{
+		if (getRoot() != "")
+			return &FileSystem::getPathRelativeRoot;
+		else
+			return &FileSystem::getPathRelativeBinary;
+	}
 
-  static std::string getPathRelativeRoot(const std::string& path)
-  {
-    return getRoot() + std::string("/") + path;
-  }
+	static std::string getPathRelativeRoot(const std::string& path)
+	{
+		return getRoot() + std::string("/") + path;
+	}
 
-  static std::string getPathRelativeBinary(const std::string& path)
-  {
-    return "../../../" + path;
-  }
+	static std::string getPathRelativeBinary(const std::string& path)
+	{
+		return "../../../" + path;
+	}
 
 
 };
