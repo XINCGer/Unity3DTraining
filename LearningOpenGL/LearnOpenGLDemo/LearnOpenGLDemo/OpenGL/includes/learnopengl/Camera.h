1a43be0b4ba5e03e85a88aa4b91f9517dd431c2f
马三小伙儿 <1790617178@qq.com>
2019-06-29 19:43:51 +0800

    抽象出一个OpenGL的Camera类

--- LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/Camera.h
+++ LearningOpenGL/LearnOpenGLDemo/LearnOpenGLDemo/OpenGL/includes/learnopengl/Camera.h
@@ -25,7 +25,97 @@ const float ZOOM = 45.0f;
 //适用于OpenGL的相机类
 class Camera {
 public:
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+
+	float Yaw;
+	float Pitch;
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		this->Position = position;
+		this->WorldUp = up;
+		this->Yaw = yaw;
+		this->Pitch = pitch;
+		this->updateCameraVectors();
+	}
+
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		this->Position = glm::vec3(posX, posY, posZ);
+		this->WorldUp = glm::vec3(upX, upY, upZ);
+		this->Yaw = yaw;
+		this->Pitch = pitch;
+		this->updateCameraVectors();
+	}
+
+	glm::mat4 GetViewMatrix()
+	{
+		return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
+	}
+
+	void ProcessKeyboard(CAMERA_MOVETYPE direction, float deltaTime)
+	{
+		float velocity = this->MovementSpeed * deltaTime;
+		if (FORWARD == direction)
+			this->Position += this->Front * velocity;
+		if (BACKWARD == direction)
+			this->Position -= this->Front * velocity;
+		if (LEFT == direction)
+			this->Position -= this->Right * velocity;
+		if (RIGHT == direction)
+			this->Position += this->Right * velocity;
+	}
+
+	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+	{
+		xoffset *= MouseSensitivity;
+		yoffset *= MouseSensitivity;
+
+		Yaw += xoffset;
+		Pitch += yoffset;
+
+		if (constrainPitch) {
+			if (Pitch > 89.0f) {
+				Pitch = 89.0f;
+			}
+			if (Pitch < -89.0f) {
+				Pitch = -89.0f;
+			}
+		}
+		this->updateCameraVectors();
+	}
+
+	void ProcessMouseScroll(float yoffset)
+	{
+		if (Zoom >= 1.0f && Zoom <= 45.0f) {
+			Zoom -= yoffset;
+		}
+		if (Zoom <= 1.0f) {
+			Zoom = 1.0f;
+		}
+		if (Zoom >= 45.0f) {
+			Zoom = 45.0f;
+		}
+	}
 private:
+	void updateCameraVectors()
+	{
+		glm::vec3 front;
+		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		front.y = sin(glm::radians(Pitch));
+		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		this->Front = glm::normalize(front);
+
+		Right = glm::normalize(glm::cross(Front, WorldUp));
+		Up = glm::normalize(glm::cross(Right, Front));
+	}
 };
 #endif // !_CAMERA_H
 
