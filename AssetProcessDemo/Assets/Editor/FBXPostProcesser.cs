0a70490311ee35dde226c97ba364eeb331a1d98f
XinCGer <1790617178@qq.com>
2020-10-10 20:06:19 +0800

    添加AssetPostProcessDemo

---
+++ AssetProcessDemo/Assets/Editor/FBXPostProcesser.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class FBXPostProcesser : AssetPostprocessor
+{
+    #region 模型处理
+    /// <summary>
+    /// 模型导入之前调用  
+    /// </summary>
+    public void OnPreprocessModel()
+    {
+        var metaPath = this.assetPath + ".meta";
+        if (File.Exists(metaPath))
+        {
+            return;
+        }
+        //if (!assetImporter.importSettingsMissing)
+        //{
+        //    return;
+        //}
+
+        Debug.Log("==模型导入之前调用==" + this.assetPath);
+        ModelImporter modelImporter = (ModelImporter)assetImporter;
+
+        //模型优化
+        modelImporter.optimizeMesh = true;
+        modelImporter.optimizeGameObjects = true;
+        modelImporter.animationCompression = ModelImporterAnimationCompression.Optimal;
+        modelImporter.animationRotationError = 1.0f;
+        modelImporter.animationPositionError = 1.0f;
+        modelImporter.animationScaleError = 1.0f;
+    }
+
+
+    /// <summary>
+    /// 模型导入之后调用  
+    /// </summary>
+    /// <param name="go"></param>
+    public void OnPostprocessModel(GameObject go)
+    {
+        var metaPath = this.assetPath + ".meta";
+        if (File.Exists(metaPath))
+        {
+            return;
+        }
+        //if (!assetImporter.importSettingsMissing)
+        //{
+        //    return;
+        //}
+
+        // for skeleton animations.
+        Debug.Log("==模型导入之后调用==");
+        List<AnimationClip> animationClipList = new List<AnimationClip>(AnimationUtility.GetAnimationClips(go));
+        if (animationClipList.Count == 0)
+        {
+            AnimationClip[] objectList = Object.FindObjectsOfType(typeof(AnimationClip)) as AnimationClip[];
+            animationClipList.AddRange(objectList);
+        }
+
+        foreach (AnimationClip theAnimation in animationClipList)
+        {
+            try
+            {
+                //  去除scale曲线
+                //foreach (EditorCurveBinding theCurveBinding in AnimationUtility.GetCurveBindings(theAnimation))
+                //{
+                //    string name = theCurveBinding.propertyName.ToLower();
+                //    if (name.Contains("scale"))
+                //    {
+                //        AnimationUtility.SetEditorCurve(theAnimation, theCurveBinding, null);
+                //    }
+                //}
+
+                // 浮点数精度压缩到f3
+                AnimationClipCurveData[] curves = null;
+                curves = AnimationUtility.GetAllCurves(theAnimation);
+                Keyframe key;
+                Keyframe[] keyFrames;
+                for (int ii = 0; ii < curves.Length; ++ii)
+                {
+                    AnimationClipCurveData curveDate = curves[ii];
+                    if (curveDate.curve == null || curveDate.curve.keys == null)
+                    {
+                        //Debuger.LogWarning(string.Format("AnimationClipCurveData {0} don't have curve; Animation name {1} ", curveDate, animationPath));
+                        continue;
+                    }
+                    keyFrames = curveDate.curve.keys;
+                    for (int i = 0; i < keyFrames.Length; i++)
+                    {
+                        key = keyFrames[i];
+                        key.value = float.Parse(key.value.ToString("f3"));
+                        key.inTangent = float.Parse(key.inTangent.ToString("f3"));
+                        key.outTangent = float.Parse(key.outTangent.ToString("f3"));
+                        keyFrames[i] = key;
+                    }
+                    curveDate.curve.keys = keyFrames;
+                    theAnimation.SetCurve(curveDate.path, curveDate.type, curveDate.propertyName, curveDate.curve);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(string.Format("CompressAnimationClip Failed !!! animationPath : {0} error: {1}", assetPath, e));
+            }
+        }
+    }
+#endregion
+
+}
